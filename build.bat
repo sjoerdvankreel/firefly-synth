70bb408af13b6e980630397f5076f6dc46139e7c
SjoerdK <Sjoerd.vanKreel@Ortec-Finance.com>
2023-08-30 13:50:32 +0200

    10

--- build.bat
+++ build.bat
@@ -1,7 +1,15 @@
 @echo off
 setlocal
+
+cd ..
 if not exist build\win mkdir build\win
 cd build\win
 cmake ../..
-msbuild
+if %errorlevel% neq 0 exit /b !errorlevel!
+
+msbuild /property:Configuration=Debug infernal-synth.sln
+if %errorlevel% neq 0 exit /b !errorlevel!
+msbuild /property:Configuration=Release infernal-synth.sln
+if %errorlevel% neq 0 exit /b !errorlevel!
+
 cd ..\..
