f9245b7b38679be57e3ac53ab18f9af11a22a8e3
sjoerdvankreel@gmail.com <Sjoerd.vanKreel@Ortec-Finance.com>
2023-09-04 19:42:38 +0200

    125

--- src/infernal.synth/infernal.synth/plugin.hpp
+++ src/infernal.synth/infernal.synth/plugin.hpp
@@ -1,6 +1,14 @@
 #pragma once
 #include <infernal.base/utility.hpp>
 
+#if INF_SYNTH_FX
+#define INF_SYNTH_NAME "Infernal Synth FX"
+#define INF_SYNTH_ID "740EA0FF92D44ABB947C6C94DEF45E2C"
+#else
+#define INF_SYNTH_NAME "Infernal Synth"
+#define INF_SYNTH_ID "56AFFFF7565F48348C4359D5E6C856A4"
+#endif
+
 #define INF_SYNTH_VENDOR_NAME "Sjoerd van Kreel"
 #define INF_SYNTH_VENDOR_MAIL "sjoerdvankreel@gmail.com"
 #define INF_SYNTH_VENDOR_URL "https://sjoerdvankreel.github.io/infernal-synth"
@@ -8,12 +16,4 @@
 #define INF_SYNTH_VERSION_MAJOR 2
 #define INF_SYNTH_VERSION_MINOR 0
 #define INF_SYNTH_VERSION_TEXT INF_VERSION_TEXT(INF_SYNTH_VERSION_MAJOR, INF_SYNTH_VERSION_MINOR)
-
-#if INF_SYNTH_FX
-#define INF_SYNTH_NAME "Infernal Synth FX";
-#define INF_SYNTH_ID "740EA0FF92D44ABB947C6C94DEF45E2C"
-#else
-#define INF_SYNTH_NAME "Infernal Synth"
-#define INF_SYNTH_ID "56AFFFF7565F48348C4359D5E6C856A4"
-#endif
 #define INF_SYNTH_FULL_NAME INF_SYNTH_NAME INF_SYNTH_VERSION_TEXT
