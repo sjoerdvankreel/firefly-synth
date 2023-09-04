cec77e7a3cb7645e43d3dd49547650cf8369843e
sjoerdvankreel@gmail.com <Sjoerd.vanKreel@Ortec-Finance.com>
2023-09-04 21:52:48 +0200

    131

---
+++ cmake/infernal.config.cmake
@@ -0,0 +1,6 @@
+cmake_policy(SET CMP0091 NEW)
+set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_EXTENSIONS OFF)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_CONFIGURATION_TYPES "Debug;Release")
+add_compile_options(/W4;/WX;/external:W0;/wd4245;/wd4267;/wd4244;/wd4100;/wd26495;/fp:fast;/arch:AVX)
