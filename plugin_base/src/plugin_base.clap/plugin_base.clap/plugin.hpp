7c1645a19c6df8bb4b6f3da6dd3513ffa528a112
sjoerdvankreel@gmail.com <Sjoerd.vanKreel@Ortec-Finance.com>
2023-09-09 22:32:45 +0200

    227

--- plugin_base/src/plugin_base.clap/plugin_base.clap/plugin.hpp
+++ plugin_base/src/plugin_base.clap/plugin_base.clap/plugin.hpp
@@ -31,13 +31,33 @@ public:
 
   std::uint32_t notePortsCount(bool is_input) const noexcept override { return is_input? 1: 0;}
   bool notePortsInfo(std::uint32_t index, bool is_input, clap_note_port_info* info) const noexcept override;
-
   std::uint32_t audioPortsCount(bool is_input) const noexcept override;
   bool audioPortsInfo(std::uint32_t index, bool is_input, clap_audio_port_info* info) const noexcept override;
 
   void deactivate() noexcept override { _engine.deactivate();  }
   clap_process_status process(clap_process const* process) noexcept override;
   bool activate(double sample_rate, std::uint32_t min_frame_count, std::uint32_t max_frame_count) noexcept override;
+
+  virtual bool implementsGui() const noexcept { return false; }
+  virtual bool guiIsApiSupported(const char* api, bool isFloating) noexcept { return false; }
+  virtual bool guiGetPreferredApi(const char** api, bool* is_floating) noexcept {
+    return false;
+  }
+  virtual bool guiCreate(const char* api, bool isFloating) noexcept { return false; }
+  virtual void guiDestroy() noexcept {}
+  virtual bool guiSetScale(double scale) noexcept { return false; }
+  virtual bool guiShow() noexcept { return false; }
+  virtual bool guiHide() noexcept { return false; }
+  virtual bool guiGetSize(uint32_t* width, uint32_t* height) noexcept { return false; }
+  virtual bool guiCanResize() const noexcept { return false; }
+  virtual bool guiGetResizeHints(clap_gui_resize_hints_t* hints) noexcept { return false; }
+  virtual bool guiAdjustSize(uint32_t* width, uint32_t* height) noexcept {
+    return guiGetSize(width, height);
+  }
+  virtual bool guiSetSize(uint32_t width, uint32_t height) noexcept { return false; }
+  virtual void guiSuggestTitle(const char* title) noexcept {}
+  virtual bool guiSetParent(const clap_window* window) noexcept { return false; }
+  virtual bool guiSetTransient(const clap_window* window) noexcept { return false; }
 };
 
 }
