0a3eb267f1d07973e1a6bfc08fcc943101fd1c8f
sjoerdvankreel <sjoerdvankreel@gmail.com>
2023-09-12 23:55:52 +0200

    297

--- plugin_base/src/plugin_base/plugin_base/support.hpp
+++ plugin_base/src/plugin_base/plugin_base/support.hpp
@@ -12,41 +12,41 @@ make_module_group(
   std::string const& id, std::string const& name,
   int module_count, module_scope scope, module_output output);
 
-// block-rate input
+// block-rate
 param_topo
 param_toggle(
   std::string const& id, std::string const& name, int group, 
-  param_text text, bool default_);
+  param_direction direction, param_text text, bool default_);
 param_topo
 param_steps(
   std::string const& id, std::string const& name, int group,
-  param_display display, param_text text,
+  param_direction direction, param_display display, param_text text,
   int min, int max, int default_);
 param_topo
 param_items(
   std::string const& id, std::string const& name, int group,
-  param_display display, param_text text,
+  param_direction direction, param_display display, param_text text,
   items_topo_factory items_factory, std::string const& default_);
 param_topo
 param_names(
   std::string const& id, std::string const& name, int group,
-  param_display display, param_text text,
+  param_direction direction, param_display display, param_text text,
   std::vector<std::string> const& names, std::string const& default_);
 
-// either block or accurate rate input
+// either block or accurate rate
 param_topo
 param_percentage(
   std::string const& id, std::string const& name, int group,
-  param_display display, param_text text, param_rate rate, bool unit,
+  param_direction direction, param_display display, param_text text, param_rate rate, bool unit,
   double min, double max, double default_);
 param_topo
 param_linear(
   std::string const& id, std::string const& name, int group,
-  param_display display, param_text text, param_rate rate,
+  param_direction direction, param_display display, param_text text, param_rate rate,
   double min, double max, double default_, std::string const& unit);
 param_topo
 param_log(
   std::string const& id, std::string const& name, int group,
-  param_display display, param_text text, param_rate rate,
+  param_direction direction, param_display display, param_text text, param_rate rate,
   double min, double max, double default_, double midpoint, std::string const& unit);
 }
