f7d02967947495127bad5f02a72a5637b50073e6
sjoerdvankreel <sjoerdvankreel@gmail.com>
2023-09-13 15:59:15 +0200

    315 take 2

--- plugin_base/src/plugin_base/plugin_base/support.cpp
+++ plugin_base/src/plugin_base/plugin_base/support.cpp
@@ -9,7 +9,7 @@ note_names()
 
 static param_topo
 param_base(
-  std::string const& id, std::string const& name, int section, std::string const& default_,
+  std::string const& id, std::string const& name, int count, int section, std::string const& default_,
   param_direction direction, param_edit edit, param_label label, param_rate rate)
 {
   param_topo result = {};
@@ -18,32 +18,33 @@ param_base(
   result.name = name;
   result.rate = rate;
   result.label = label;
+  result.count = count;
   result.section = section;
   result.direction = direction;
   result.default_text = default_;
   return result;
 }
 
-module_group_topo
-make_module_group(
+module_topo
+make_module(
   std::string const& id, std::string const& name,
-  int module_count, module_scope scope, module_output output)
+  int count, module_scope scope, module_output output)
 {
-  module_group_topo result = {};
+  module_topo result = {};
   result.id = id;
   result.name = name;
+  result.count = count;
   result.scope = scope;
   result.output = output;
-  result.module_count = module_count;
   return result;
 }
 
 param_topo
 param_toggle(
-  std::string const& id, std::string const& name, int section,
+  std::string const& id, std::string const& name, int count, int section,
   param_direction direction, param_label label, bool default_)
 {
-  param_topo result(param_base(id, name, section, default_? "On": "Off", direction, param_edit::toggle, label, param_rate::block));
+  param_topo result(param_base(id, name, count, section, default_? "On": "Off", direction, param_edit::toggle, label, param_rate::block));
   result.min = 0;
   result.max = 1;
   result.type = param_type::step;
@@ -52,11 +53,11 @@ param_toggle(
 
 param_topo
 param_steps(
-  std::string const& id, std::string const& name, int section,
+  std::string const& id, std::string const& name, int count, int section,
   param_direction direction, param_edit edit, param_label label,
   int min, int max, int default_)
 {
-  param_topo result(param_base(id, name, section, std::to_string(default_), direction, edit, label, param_rate::block));
+  param_topo result(param_base(id, name, count, section, std::to_string(default_), direction, edit, label, param_rate::block));
   result.min = min;
   result.max = max;
   result.type = param_type::step;
@@ -65,11 +66,11 @@ param_steps(
 
 param_topo
 param_items(
-  std::string const& id, std::string const& name, int section,
+  std::string const& id, std::string const& name, int count, int section,
   param_direction direction, param_edit edit, param_label label,
   items_topo_factory items_factory, std::string const& default_)
 {
-  param_topo result(param_base(id, name, section, default_, direction, edit, label, param_rate::block));
+  param_topo result(param_base(id, name, count, section, default_, direction, edit, label, param_rate::block));
   result.items = items_factory();
   result.min = 0;
   result.max = result.items.size() - 1;
@@ -79,11 +80,11 @@ param_items(
 
 param_topo
 param_names(
-  std::string const& id, std::string const& name, int section,
+  std::string const& id, std::string const& name, int count, int section,
   param_direction direction, param_edit edit, param_label label,
   std::vector<std::string> const& names, std::string const& default_)
 {
-  param_topo result(param_base(id, name, section, default_, direction, edit, label, param_rate::block));
+  param_topo result(param_base(id, name, count, section, default_, direction, edit, label, param_rate::block));
   result.min = 0;
   result.max = names.size() - 1;
   result.names = names;
@@ -93,11 +94,11 @@ param_names(
 
 param_topo
 param_percentage(
-  std::string const& id, std::string const& name, int section,
+  std::string const& id, std::string const& name, int count, int section,
   param_direction direction, param_edit edit, param_label label, param_rate rate, bool unit,
   double min, double max, double default_)
 {
-  param_topo result(param_base(id, name, section, std::to_string(default_ * 100), direction, edit, label, rate));
+  param_topo result(param_base(id, name, count, section, std::to_string(default_ * 100), direction, edit, label, rate));
   result.min = min;
   result.max = max;
   result.unit = unit? "%": "";
@@ -108,11 +109,11 @@ param_percentage(
 
 param_topo
 param_linear(
-  std::string const& id, std::string const& name, int section,
+  std::string const& id, std::string const& name, int count, int section,
   param_direction direction, param_edit edit, param_label label, param_rate rate,
   double min, double max, double default_, std::string const& unit)
 {
-  param_topo result(param_base(id, name, section, std::to_string(default_), direction, edit, label, rate));
+  param_topo result(param_base(id, name, count, section, std::to_string(default_), direction, edit, label, rate));
   result.min = min;
   result.max = max;
   result.unit = unit;
@@ -122,11 +123,11 @@ param_linear(
 
 param_topo
 param_log(
-  std::string const& id, std::string const& name, int section,
+  std::string const& id, std::string const& name, int count, int section,
   param_direction direction, param_edit edit, param_label label, param_rate rate,
   double min, double max, double default_, double midpoint, std::string const& unit)
 {
-  param_topo result(param_base(id, name, section, std::to_string(default_), direction, edit, label, rate));
+  param_topo result(param_base(id, name, count, section, std::to_string(default_), direction, edit, label, rate));
   result.min = min;
   result.max = max;
   result.unit = unit;
