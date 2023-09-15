a6438b90f56d9967766a97b8c491de07eeec8508
sjoerdvankreel <sjoerdvankreel@gmail.com>
2023-09-15 22:23:41 +0200

    404

--- plugin_base/src/plugin_base/plugin_base/engine.hpp
+++ plugin_base/src/plugin_base/plugin_base/engine.hpp
@@ -12,14 +12,17 @@
 
 namespace plugin_base {
 
-// module audio processor fills its own audio/cv output (if any)
-// final module engine is responsible to fill host audio output
+// single module audio processor
 class module_engine {
 public:
   virtual void 
-  process(plugin_topo const& topo, plugin_block const& plugin, module_block& module) = 0;
+  process(
+    plugin_topo const& topo, 
+    plugin_block const& plugin, 
+    module_block& module) = 0;
 };
 
+// global plugin audio processor
 class plugin_engine final {
   plugin_desc const _desc;
   plugin_dims const _dims; 
@@ -28,21 +31,20 @@ class plugin_engine final {
   plugin_block _plugin_block = {};
   common_block _common_block = {};
   jarray<plain_value, 4> _state = {};
-  std::vector<int> _accurate_frames = {}; // track accurate event frame positions per parameter
-  std::chrono::milliseconds _activated_at_ms = {}; // dont push output params too often
+  std::vector<int> _accurate_frames = {};
+  std::chrono::milliseconds _activated_at_ms = {};
   jarray<std::unique_ptr<module_engine>, 2> _module_engines = {};
 
 public:
-  INF_DECLARE_MOVE_ONLY(plugin_engine);
-  explicit plugin_engine(std::unique_ptr<plugin_topo>&& topo);
-
   void process();
   void deactivate();
   host_block& prepare();
   void activate(int sample_rate, int max_frame_count);
 
+  INF_DECLARE_MOVE_ONLY(plugin_engine);
+  explicit plugin_engine(std::unique_ptr<plugin_topo>&& topo);
+
   plugin_desc const& desc() const { return _desc; }
-  // CLAP needs direct write access to the audio thread.
   jarray<plain_value, 4>& state() { return _state; }
   jarray<plain_value, 4> const& state() const { return _state; }
 };
