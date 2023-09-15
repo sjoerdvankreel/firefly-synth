fb8e32df81fb7eb7d701fc14957b58467a50e073
sjoerdvankreel <sjoerdvankreel@gmail.com>
2023-09-15 19:08:47 +0200

    383

--- plugin_base/src/plugin_base/plugin_base/desc.cpp
+++ plugin_base/src/plugin_base/plugin_base/desc.cpp
@@ -4,7 +4,7 @@
 
 namespace plugin_base {
 
-// nonnegative int, required for vst3 param tags
+// nonnegative required for vst3 param tags
 static int
 stable_hash(std::string const& text)
 {
@@ -16,34 +16,34 @@ stable_hash(std::string const& text)
 }
 
 static std::string
-module_id(module_topo const& module, int slot_index)
+module_id(module_topo const& module, int slot)
 {
   std::string result = module.id;
-  result += "-" + std::to_string(slot_index);
+  result += "-" + std::to_string(slot);
   return result;
 }
 
 static std::string
-module_name(module_topo const& module, int slot_index)
+module_name(module_topo const& module, int slot)
 {
   std::string result = module.name;
-  if(module.slot_count > 1) result += " " + std::to_string(slot_index + 1);
+  if(module.slot_count > 1) result += " " + std::to_string(slot + 1);
   return result;
 }
 
 static std::string
-param_id(param_topo const& param, int slot_index)
+param_id(param_topo const& param, int slot)
 {
   std::string result = param.id;
-  result += "-" + std::to_string(slot_index);
+  result += "-" + std::to_string(slot);
   return result;
 }
 
 static std::string
-param_name(param_topo const& param, int slot_index)
+param_name(param_topo const& param, int slot)
 {
   std::string result = param.name;
-  if (param.slot_count > 1) result += " " + std::to_string(slot_index + 1);
+  if (param.slot_count > 1) result += " " + std::to_string(slot + 1);
   return result;
 }
 
@@ -55,15 +55,15 @@ validate_desc(plugin_desc const& desc)
   std::set<int> all_hashes;
   std::set<std::string> all_ids;
 
-  assert(desc.module_global_count > 0);
-  assert(desc.modules.size() == desc.module_global_count);
-  assert(desc.param_global_count > 0);
-  assert(desc.param_mappings.size() == desc.param_global_count);
-  assert(desc.param_id_to_index.size() == desc.param_global_count);
-  assert(desc.param_index_to_id.size() == desc.param_global_count);
+  assert(desc.param_count > 0);
+  assert(desc.module_count > 0);
+  assert(desc.modules.size() == desc.module_count);
+  assert(desc.param_mappings.size() == desc.param_count);
+  assert(desc.param_id_to_index.size() == desc.param_count);
+  assert(desc.param_index_to_id.size() == desc.param_count);
 
-  int param_global_index = 0;
-  (void)param_global_index;
+  int param_global = 0;
+  (void)param_global;
   for (int m = 0; m < desc.modules.size(); m++)
   {
     auto const& module = desc.modules[m];
@@ -72,11 +72,11 @@ validate_desc(plugin_desc const& desc)
     assert(module.name.size());
     assert(module.params.size());
     assert(module.id_hash >= 0);
-    assert(module.global_index == m);
-    assert(module.topo_index >= 0);
-    assert(module.topo_index < desc.topo->modules.size());
-    assert(module.slot_index >= 0);
-    assert(module.slot_index < module.topo->slot_count);
+    assert(module.global == m);
+    assert(module.topo >= 0);
+    assert(module.topo < desc.plugin->modules.size());
+    assert(module.slot >= 0);
+    assert(module.slot < module.module->slot_count);
     INF_ASSERT_EXEC(all_ids.insert(module.id).second);
     INF_ASSERT_EXEC(all_hashes.insert(module.id_hash).second);
 
@@ -88,13 +88,13 @@ validate_desc(plugin_desc const& desc)
       assert(param.id.size() > 0);
       assert(param.short_name.size() > 0);
       assert(param.short_name.size() < param.full_name.size());
-      assert(param.slot_index >= 0);
-      assert(param.slot_index < param.topo->slot_count);
-      assert(param.local_index >= 0);
-      assert(param.local_index < module.params.size());
-      assert(param.topo_index >= 0);
-      assert(param.topo_index < module.topo->params.size());
-      assert(param.global_index == param_global_index++);
+      assert(param.slot >= 0);
+      assert(param.slot < param.param->slot_count);
+      assert(param.local >= 0);
+      assert(param.local < module.params.size());
+      assert(param.topo >= 0);
+      assert(param.topo < module.module->params.size());
+      assert(param.global == param_global++);
       INF_ASSERT_EXEC(all_ids.insert(param.id).second);
       INF_ASSERT_EXEC(all_hashes.insert(param.id_hash).second);
     }
@@ -217,53 +217,51 @@ validate_topo(plugin_topo const& topo)
 
 param_desc::
 param_desc(
-  module_topo const& module, int module_slot_index,
-  param_topo const& param, int topo_index, int slot_index,
-  int local_index, int global_index)
+  module_topo const& module_, int module_slot,
+  param_topo const& param_, int topo, int slot, int local, int global)
 {
-  topo = &param;
-  this->topo_index = topo_index;
-  this->slot_index = slot_index;
-  this->local_index = local_index;
-  this->global_index = global_index;
-  short_name = param_name(param, slot_index);
-  full_name = module_name(module, module_slot_index) + " " + short_name;
-  id = module_id(module, module_slot_index) + "-" + param_id(param, slot_index);
+  param = &param_;
+  this->topo = topo;
+  this->slot = slot;
+  this->local = local;
+  this->global = global;
+  short_name = param_name(param_, slot);
+  full_name = module_name(module_, module_slot) + " " + short_name;
+  id = module_id(module_, module_slot) + "-" + param_id(param_, slot);
   id_hash = stable_hash(id.c_str());
 }
 
 module_desc::
 module_desc(
-  module_topo const& module, int topo_index, int slot_index,
-  int global_index, int param_global_index_start)
+  module_topo const& module_, int topo, int slot, int global, int param_global_start)
 {
-  topo = &module;
-  int param_local_index = 0;
-  this->topo_index = topo_index;
-  this->slot_index = slot_index;
-  this->global_index = global_index;
-  id = module_id(module, slot_index);
-  name = module_name(module, slot_index);
+  module = &module_;
+  int param_local = 0;
+  this->topo = topo;
+  this->slot = slot;
+  this->global = global;
+  id = module_id(module_, slot);
+  name = module_name(module_, slot);
   id_hash = stable_hash(id);
-  for(int p = 0; p < module.params.size(); p++)
+  for(int p = 0; p < module_.params.size(); p++)
   {
-    auto const& param = module.params[p];
+    auto const& param = module_.params[p];
     for(int i = 0; i < param.slot_count; i++)
-      params.emplace_back(param_desc(module, slot_index, param, p, i, param_local_index++, param_global_index_start++));
+      params.emplace_back(param_desc(module_, slot, param, p, i, param_local++, param_global_start++));
   }
 }
 
 plugin_desc::
-plugin_desc(std::unique_ptr<plugin_topo>&& topo_):
-topo(std::move(topo_))
+plugin_desc(std::unique_ptr<plugin_topo>&& plugin_):
+plugin(std::move(plugin_))
 {
-  validate_topo(*topo);
+  validate_topo(*plugin);
 
   int param_global_index = 0;
   int module_global_index = 0;
-  for(int m = 0; m < topo->modules.size(); m++)
+  for(int m = 0; m < plugin->modules.size(); m++)
   {
-    auto const& module = topo->modules[m];
+    auto const& module = plugin->modules[m];
     for(int i = 0; i < module.slot_count; i++)
     {
       modules.emplace_back(module_desc(module, m, i, module_global_index++, param_global_index));
@@ -280,30 +278,30 @@ topo(std::move(topo_))
     {
       auto const& param = module.params[p];
       param_mapping mapping;
-      mapping.module_global_index = m;
-      mapping.param_local_index = p;
-      mapping.param_global_index = param_global_index++;
-      mapping.param_slot_index = param.slot_index;
-      mapping.param_topo_index = param.topo_index;
-      mapping.module_slot_index = module.slot_index;
-      mapping.module_topo_index = module.topo_index;
+      mapping.module_global = m;
+      mapping.param_local = p;
+      mapping.param_global = param_global_index++;
+      mapping.param_slot = param.slot;
+      mapping.param_topo = param.topo;
+      mapping.module_slot = module.slot;
+      mapping.module_topo = module.topo;
       param_index_to_id.push_back(param.id_hash);
       param_id_to_index[param.id_hash] = param_mappings.size();
       param_mappings.push_back(std::move(mapping));
     }
   }
 
-  module_global_count = modules.size();
-  param_global_count = param_global_index;
+  module_count = modules.size();
+  param_count = param_global_index;
   validate_desc(*this);
 }
 
 void
 plugin_desc::init_default_state(jarray<plain_value, 4>& state) const
 {
-  for (int m = 0; m < topo->modules.size(); m++)
+  for (int m = 0; m < plugin->modules.size(); m++)
   {
-    auto const& module = topo->modules[m];
+    auto const& module = plugin->modules[m];
     for(int mi = 0; mi < module.slot_count; mi++)
       for (int p = 0; p < module.params.size(); p++)
       {
--- plugin_base/src/plugin_base/plugin_base/desc.hpp
+++ plugin_base/src/plugin_base/plugin_base/desc.hpp
@@ -13,73 +13,74 @@ namespace plugin_base {
 
 // mapping plugin level parameter index
 struct param_mapping final {
-  int param_topo_index = {};
-  int param_slot_index = {};
-  int param_local_index = {};
-  int param_global_index = {};
-  int module_topo_index = {};
-  int module_slot_index = {};
-  int module_global_index = {};
+  int param_topo = {};
+  int param_slot = {};
+  int param_local = {};
+  int param_global = {};
+  int module_topo = {};
+  int module_slot = {};
+  int module_global = {};
   INF_DECLARE_MOVE_ONLY(param_mapping);
 
   template <class T> auto& value_at(T& container) const 
-  { return container[module_topo_index][module_slot_index][param_topo_index][param_slot_index]; }
+  { return container[module_topo][module_slot][param_topo][param_slot]; }
   template <class T> auto const& value_at(T const& container) const 
-  { return container[module_topo_index][module_slot_index][param_topo_index][param_slot_index]; }
+  { return container[module_topo][module_slot][param_topo][param_slot]; }
 };
 
 // runtime parameter descriptor
 struct param_desc final {
+  int topo = {};
+  int slot = {};
+  int local = {};
+  int global = {};
   int id_hash = {};
-  int topo_index = {};
-  int slot_index = {};
-  int local_index = {};
-  int global_index = {};
   std::string id = {};
   std::string full_name = {};
   std::string short_name = {};
-  param_topo const* topo = {};
+  param_topo const* param = {};
 
   INF_DECLARE_MOVE_ONLY(param_desc);
   param_desc(
-    module_topo const& module, int module_slot_index,
-    param_topo const& param, int topo_index, int slot_index, 
-    int local_index, int global_index);
+    module_topo const& module_, int module_slot,
+    param_topo const& param_, int topo, int slot, int local, int global);
 };
 
 // runtime module descriptor
 struct module_desc final {
+  int topo = {};
+  int slot = {};
+  int global = {};
   int id_hash = {};
-  int topo_index = {};
-  int slot_index = {};
-  int global_index = {};
   std::string id = {};
   std::string name = {};
-  module_topo const* topo = {};
+  module_topo const* module = {};
   std::vector<param_desc> params = {};
 
   INF_DECLARE_MOVE_ONLY(module_desc);
   module_desc(
-    module_topo const& module, int topo_index, int slot_index, 
-    int global_index, int param_global_index_start);
+    module_topo const& module_, int topo, int slot, int global, int param_global_start);
 };
 
 // runtime plugin descriptor
 struct plugin_desc final {
-  int param_global_count = {};
-  int module_global_count = {};
+  int param_count = {};
+  int module_count = {};
   std::vector<module_desc> modules = {};
-  std::unique_ptr<plugin_topo> topo = {};
+  std::unique_ptr<plugin_topo> plugin = {};
+
+  // TODO rename these
+  std::vector<param_mapping> param_mappings = {};
   std::vector<int> param_index_to_id = {};
   std::map<int, int> param_id_to_index = {};
-  std::vector<param_mapping> param_mappings = {};
 
   INF_DECLARE_MOVE_ONLY(plugin_desc);
-  plugin_desc(std::unique_ptr<plugin_topo>&& topo_);
+  plugin_desc(std::unique_ptr<plugin_topo>&& plugin_);
 
+  // TODO and this
   void init_default_state(jarray<plain_value, 4>& state) const;
   param_desc const& param_at(param_mapping const& mapping) const
-  { return modules[mapping.module_global_index].params[mapping.param_local_index]; }
+  { return modules[mapping.module_global].params[mapping.param_local]; }
 };
 
 // runtime plugin topo dimensions
