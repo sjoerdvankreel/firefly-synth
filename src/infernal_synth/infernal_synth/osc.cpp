ce3689e0998e39cbbb983f7cb2105d4cd6ef6dec
sjoerdvankreel <sjoerdvankreel@gmail.com>
2023-09-20 16:16:08 +0200

    547

--- src/infernal_synth/infernal_synth/osc.cpp
+++ src/infernal_synth/infernal_synth/osc.cpp
@@ -42,7 +42,7 @@ osc_topo()
     return std::make_unique<osc_engine>(); };
 
   result.sections.emplace_back(make_section(
-    "Main", section_main, gui_position { 0, 0 }, gui_dimension { 1, 3 }));
+    "Main", section_main, gui_position { 0, 0 }, gui_dimension { 1, 4 }));
   result.params.emplace_back(param_toggle(
     "{AA9D7DA6-A719-4FDA-9F2E-E00ABB784845}", "On", 1, 
     section_main, param_dir::input, param_label::default_, false,
@@ -55,25 +55,25 @@ osc_topo()
     "{75E49B1F-0601-4E62-81FD-D01D778EDCB5}", "Gain", 1, 
     section_main, param_dir::input, param_edit::knob, param_label::default_, param_rate::accurate, true, 0, 1, 1,
     gui_layout::single, gui_position { 0, 2 }));
+  result.params.emplace_back(param_pct(
+    "{23C6BC03-0978-4582-981B-092D68338ADA}", "Bal", 1,
+    section_main, param_dir::input, param_edit::knob, param_label::default_, param_rate::accurate, true, -1, 1, 0,
+    gui_layout::single, gui_position { 0, 3 }));
 
   result.sections.emplace_back(make_section(
-    "Pitch", section_pitch, gui_position{ 1, 0 }, gui_dimension { 1, 4 }));
-  result.params.emplace_back(param_pct(
-    "{23C6BC03-0978-4582-981B-092D68338ADA}", "Bal", 1, 
-    section_pitch, param_dir::input, param_edit::knob, param_label::default_, param_rate::accurate, true, -1, 1, 0,
-    gui_layout::single, gui_position { 0, 0 }));
+    "Pitch", section_pitch, gui_position{ 1, 0 }, gui_dimension { 1, 3 }));
   result.params.emplace_back(param_steps(
     "{38C78D40-840A-4EBE-A336-2C81D23B426D}", "Oct", 1, 
     section_pitch, param_dir::input, param_edit::list, param_label::default_, 0, 9, 4,
-    gui_layout::single, gui_position { 0, 1 }));
+    gui_layout::single, gui_position { 0, 0 }));
   result.params.emplace_back(param_names(
     "{78856BE3-31E2-4E06-A6DF-2C9BB534789F}", "Note", 1, 
     section_pitch, param_dir::input, param_edit::text, param_label::default_, note_names(), "C",
-    gui_layout::single, gui_position { 0, 2 }));
+    gui_layout::single, gui_position { 0, 1 }));
   result.params.emplace_back(param_pct(
     "{691F82E5-00C8-4962-89FE-9862092131CB}", "Cent", 1, 
     section_pitch, param_dir::input, param_edit::knob, param_label::default_, param_rate::accurate, false, -1, 1, 0,
-    gui_layout::single, gui_position { 0, 3 }));
+    gui_layout::single, gui_position { 0, 2 }));
 
   return result;
 }
