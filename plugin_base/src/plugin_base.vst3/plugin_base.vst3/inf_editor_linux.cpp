a0381d6bf0a555210fe94271b12f3b4f0100327c
sjoerdvankreel <sjoerdvankreel@gmail.com>
2023-09-22 01:04:24 +0200

    583

--- plugin_base/src/plugin_base.vst3/plugin_base.vst3/inf_editor_linux.cpp
+++ plugin_base/src/plugin_base.vst3/plugin_base.vst3/inf_editor_linux.cpp
@@ -22,21 +22,25 @@ struct inf_editor_linux::impl
 
 inf_editor_linux::
 inf_editor_linux(inf_controller* controller):
-inf_editor(controller), _impl(std::make_unique<impl>())
-{ 
+inf_editor(controller), _impl(std::make_unique<impl>()) 
+{
   const MessageManagerLock mm_lock;
   _impl->gui.reset(new plugin_gui(&controller->desc(), &controller->ui_state()));
 }
 
 plugin_gui* 
 inf_editor_linux::gui() const
-{ return _impl->gui.get(); }
+{ 
+  assert(_impl->gui.get());
+  return _impl->gui.get(); 
+}
 
 tresult PLUGIN_API 
 inf_editor_linux::removed()
 {
   _impl->gui->remove_ui_listener(_controller);
   _impl->gui->setVisible(false);
+  _impl->gui.reset();
   _impl->event_handler->unregisterHandlerForFrame(plugFrame);
   return EditorView::removed();
 }
@@ -44,11 +48,18 @@ inf_editor_linux::removed()
 tresult PLUGIN_API 
 inf_editor_linux::attached(void* parent, FIDString type)
 {
+  if (parent == nullptr) return kResultFalse;
   _impl->event_handler->registerHandlerForFrame(plugFrame);
+  systemWindow = parent;
+  if(!_impl->gui) 
+  {
+    MessageManagerLock const mm_lock;
+    _impl->gui.reset(new plugin_gui(&_controller->desc(), &_controller->ui_state()));
+  }
   _impl->gui->addToDesktop(0, parent);
   _impl->gui->setVisible(true);
   _impl->gui->add_ui_listener(_controller);
-  return EditorView::attached(parent, type);
+  return kResultTrue;
 }
 
 }
