85007cdcb93a7d866fdbf6787385aa131c467eb2
sjoerdvankreel <sjoerdvankreel@gmail.com>
2023-09-21 20:11:23 +0200

    572

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,10 +7,12 @@ cd build/linux/debug
 cmake -DCMAKE_BUILD_TYPE=Debug ../../..
 make
 
+if [ "$1" -eq "1" ] ; then
 cd ..
 mkdir -p release
 cd release
 cmake -DCMAKE_BUILD_TYPE=Release ../../..
 make
+fi
 
 cd ../../../scripts
