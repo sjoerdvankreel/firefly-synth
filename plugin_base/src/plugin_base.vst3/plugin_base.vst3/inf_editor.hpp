e04bdb5869cbde3b30a107ecaef2983098f7184a
sjoerdvankreel <sjoerdvankreel@gmail.com>
2023-09-22 23:28:21 +0200

    Revert "579 prepare to tryout linux ui once more"
    This reverts commit 7ebbcb8fad69fac6cb2b0dfe46d4220fc7a8fa20.

--- plugin_base/src/plugin_base.vst3/plugin_base.vst3/inf_editor.hpp
+++ plugin_base/src/plugin_base.vst3/plugin_base.vst3/inf_editor.hpp
@@ -5,33 +5,33 @@
 
 #include <public.sdk/source/vst/vsteditcontroller.h>
 #include <pluginterfaces/gui/iplugviewcontentscalesupport.h>
-
 #include <utility>
 
 namespace plugin_base::vst3 {
 
-class inf_editor:
+class inf_editor final:
 public Steinberg::Vst::EditorView,
 public Steinberg::IPlugViewContentScaleSupport
 {
-protected:
-  virtual plugin_gui* gui() const = 0;
   inf_controller* const _controller = {};
-  inf_editor(inf_controller* controller): 
-  EditorView(controller), _controller(controller) {}
+  std::unique_ptr<plugin_gui> _gui = {};
 
 public: 
   INF_DECLARE_MOVE_ONLY(inf_editor);
-  void plugin_changed(int index, plain_value plain) { gui()->plugin_changed(index, plain); }
+  inf_editor(inf_controller* controller);
+
+  void plugin_changed(int index, plain_value plain) { _gui->plugin_changed(index, plain); }
 
   Steinberg::uint32 PLUGIN_API addRef() override { return EditorView::addRef(); }
   Steinberg::uint32 PLUGIN_API release() override { return EditorView::release(); } 
   Steinberg::tresult PLUGIN_API setContentScaleFactor(float factor) override;
   Steinberg::tresult PLUGIN_API queryInterface(Steinberg::TUID const iid, void** obj) override;
 
+  Steinberg::tresult PLUGIN_API removed() override;
   Steinberg::tresult PLUGIN_API onSize(Steinberg::ViewRect* new_size) override;
   Steinberg::tresult PLUGIN_API getSize(Steinberg::ViewRect* new_size) override;
   Steinberg::tresult PLUGIN_API checkSizeConstraint(Steinberg::ViewRect* rect) override;
+  Steinberg::tresult PLUGIN_API attached(void* parent, Steinberg::FIDString type) override;
   Steinberg::tresult PLUGIN_API canResize() override { return Steinberg::kResultTrue; }
   Steinberg::tresult PLUGIN_API isPlatformTypeSupported(Steinberg::FIDString type) override  { return Steinberg::kResultTrue; }
 };
