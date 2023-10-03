13d516697b1111a42816dc524cd7178abcdf57e2
sjoerdvankreel <sjoerdvankreel@gmail.com>
2023-10-03 23:19:38 +0200

    643

--- src/infernal_synth/infernal_synth/cv_matrix.cpp
+++ src/infernal_synth/infernal_synth/cv_matrix.cpp
@@ -23,6 +23,15 @@ source_items(module_topo const& lfo_topo, module_topo const& env_topo)
   return result;
 }
 
+static std::vector<item_topo>
+target_items(module_topo const& osc_topo, module_topo const& filter_topo)
+{
+  std::vector<item_topo> result;
+  result.emplace_back(osc_topo);
+  result.emplace_back(filter_topo);
+  return result;
+}
+
 class cv_matrix_engine: 
 public module_engine { 
 public:
@@ -45,7 +54,7 @@ cv_matrix_topo(
   result.engine_factory = [](int, int, int) -> 
     std::unique_ptr<module_engine> { return std::make_unique<cv_matrix_engine>(); };
   result.sections.emplace_back(make_section(
-    "Main", section_main, gui_position { 0, 0 }, gui_dimension { { 1, 5 }, { 1, 1, 1 } }));
+    "Main", section_main, gui_position { 0, 0 }, gui_dimension { { 1, 5 }, { 1, 1, 1, 1 } }));
   
   std::vector<int> enabled_params = { cv_matrix_param_on, cv_matrix_param_active };
   ui_state_selector enabled_selector = [](auto const& vs, auto const&) { return vs[0] != 0 && vs[1] != 0; };
@@ -54,7 +63,7 @@ cv_matrix_topo(
     "{06512F9B-2B49-4C2E-BF1F-40070065CABB}", "On", cv_matrix_param_on, 1, section_main, true,
     param_dir::input,
     param_label_contents::name, param_label_align::left, param_label_justify::center,
-    gui_layout::single, gui_position { 0, 0, 1, 3 }));
+    gui_layout::single, gui_position { 0, 0, 1, 4 }));
 
   auto& active = result.params.emplace_back(param_toggle(
     "{4DF9B283-36FC-4500-ACE6-4AEBF74BA694}", "Active", cv_matrix_param_active, route_count, section_main, false,
@@ -93,6 +102,14 @@ cv_matrix_topo(
   env_index.ui_state.visibility_params = { cv_matrix_param_source };
   env_index.ui_state.visibility_context = { index_of_item_tag(result.params[cv_matrix_param_source].items, module_env) };
   env_index.ui_state.visibility_selector = [](auto const& vs, auto const& ctx) { return vs[0] == ctx[0]; };
+
+  auto& target = result.params.emplace_back(param_items(
+    "{94A037CE-F410-4463-8679-5660AFD1582E}", "Target", cv_matrix_param_target, route_count, section_main, target_items(osc_topo, filter_topo), "",
+    param_dir::input, param_edit::list,
+    param_label_contents::none, param_label_align::left, param_label_justify::center,
+    gui_layout::vertical, gui_position{ 1, 3 }));
+  target.ui_state.enabled_params = enabled_params;
+  target.ui_state.enabled_selector = enabled_selector;
 
   return result;
 }
