77fff0d7fdba2629a75b2a71d8cc490050e448da
sjoerdvankreel <sjoerdvankreel@gmail.com>
2023-10-04 17:20:06 +0200

    672

--- plugin_base/cmake/plugin_base.utility.cmake
+++ plugin_base/cmake/plugin_base.utility.cmake
@@ -5,9 +5,8 @@ function(declare_local_target LIBRARY_TYPE PROJECT_NAME SOURCE_DIR)
   target_include_directories(${PROJECT_NAME} PRIVATE "${${PROJECT_NAME}_SRC_DIR}")
 #  source_group(TREE "${CMAKE_CURRENT_SOURCE_DIR}/${${PROJECT_NAME}_SRC_DIR}" FILES "${${PROJECT_NAME}_SRC}")
 
-set(bsdir "${CMAKE_CURRENT_SOURCE_DIR}/src/${SOURCE_DIR}")
-file(GLOB_RECURSE bssrc "${bsdir}/*.*")
-source_group(TREE ${bsdir} FILES ${bssrc})
+file(GLOB_RECURSE bssrc "${${PROJECT_NAME}_SRC_DIR}/*.*")
+source_group(TREE ${${PROJECT_NAME}_SRC_DIR} FILES ${bssrc})
 
 endfunction()
 
