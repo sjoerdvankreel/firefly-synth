fa8086648c275a74e60f57d5c44a162eeac1ae82
sjoerdvankreel <sjoerdvankreel@gmail.com>
2023-10-04 17:16:12 +0200

    670

--- plugin_base/cmake/plugin_base.utility.cmake
+++ plugin_base/cmake/plugin_base.utility.cmake
@@ -3,7 +3,12 @@ function(declare_local_target LIBRARY_TYPE PROJECT_NAME SOURCE_DIR)
   file(GLOB_RECURSE "${PROJECT_NAME}_SRC" "${${PROJECT_NAME}_SRC_DIR}/*.*")
   add_library(${PROJECT_NAME} ${LIBRARY_TYPE} "${${PROJECT_NAME}_SRC}")
   target_include_directories(${PROJECT_NAME} PRIVATE "${${PROJECT_NAME}_SRC_DIR}")
-  source_group(TREE "${CMAKE_CURRENT_SOURCE_DIR}/${${PROJECT_NAME}_SRC_DIR}" FILES "${${PROJECT_NAME}_SRC}")
+#  source_group(TREE "${CMAKE_CURRENT_SOURCE_DIR}/${${PROJECT_NAME}_SRC_DIR}" FILES "${${PROJECT_NAME}_SRC}")
+
+set(bsdir "${CMAKE_CURRENT_SOURCE_DIR}/src/${SOURCE_DIR}")
+file(GLOB_RECURSE bssrc "${bsdir}/*.*")
+source_group(TREE ${bsdir} FILES ${bssrc})
+
 endfunction()
 
 function(declare_vst3_target WRAPPER_NAME WRAPPER_SRC PLUGIN_NAME IS_FX)
