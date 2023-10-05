ac18871c7f277afc89aec7a3318d76737a672c8f
sjoerdvankreel <sjoerdvankreel@gmail.com>
2023-10-05 21:04:35 +0200

    700

--- plugin_base/src/plugin_base/plugin_base/topo/param.cpp
+++ plugin_base/src/plugin_base/plugin_base/topo/param.cpp
@@ -7,18 +7,22 @@ void
 param_topo::validate(module_topo const& module) const
 {
   info.validate();
+  domain.validate();
+  gui.bindings.validate(module, info.slot_count);
+  gui.position.validate(module.sections[section].gui.dimension);
+
+  for (int e = 0; e < gui.bindings.enabled.params.size(); e++)
+    assert(info.index != gui.bindings.enabled.params[e]);
+  for (int v = 0; v < gui.bindings.visible.params.size(); v++)
+    assert(info.index != gui.bindings.visible.params[v]);
 
   assert(0 <= section && section < module.sections.size());
   assert(domain.is_real() || dsp.rate == param_rate::block);
   assert(dsp.format == param_format::plain || domain.is_real());
+  assert((info.slot_count == 1) == (gui.layout == gui_layout::single));
   assert(dsp.direction != param_direction::output || dsp.rate == param_rate::block);
   assert(gui.edit_type != gui_edit_type::toggle || domain.type == domain_type::toggle);
-
-  domain.validate();
-  gui.bindings.validate(module, info.slot_count);
   assert(dsp.direction == param_direction::input || gui.bindings.enabled.selector == nullptr);
-  assert((info.slot_count == 1) == (gui.layout == gui_layout::single));
-  gui.position.validate(module.sections[section].gui.dimension);
 }
 
 }
