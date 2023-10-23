9ebe2c336ee446d1a7d7e1038efce3a796a01242
sjoerdvankreel <sjoerdvankreel@gmail.com>
2023-10-23 19:42:39 +0200

    968

--- src/infernal_synth/infernal_synth/synth.cpp
+++ src/infernal_synth/infernal_synth/synth.cpp
@@ -10,7 +10,7 @@ namespace infernal_synth {
 
 enum { 
   section_lfos, section_env, section_osc, section_filter, 
-  section_cv_matrix, section_delay_monitor, section_count  };
+  section_cv_matrix, section_delay, section_monitor, section_count  };
 
 std::unique_ptr<plugin_topo>
 synth_topo()
@@ -29,24 +29,25 @@ synth_topo()
   result->gui.default_width = 1000;
   result->gui.aspect_ratio_width = 12;
   result->gui.aspect_ratio_height = 5;
-  result->gui.dimension.row_sizes = std::vector<int>(4, 1);
+  result->gui.dimension.row_sizes = std::vector<int>(5, 1);
   result->gui.dimension.column_sizes = std::vector<int>(2, 1);
 
   result->gui.sections.resize(section_count);
   result->gui.sections[section_env] = make_module_section_gui(section_env, { 1, 0 }, { 1, 1 });
   result->gui.sections[section_osc] = make_module_section_gui(section_osc, { 2, 0 }, { 1, 1 });
   result->gui.sections[section_lfos] = make_module_section_gui(section_lfos, { 0, 0 }, { 1, 2 });
+  result->gui.sections[section_delay] = make_module_section_gui(section_delay, { 4, 0 }, { 1, 1 });
   result->gui.sections[section_filter] = make_module_section_gui(section_filter, { 3, 0 }, { 1, 1 });
-  result->gui.sections[section_cv_matrix] = make_module_section_gui(section_cv_matrix, { 0, 1, 3, 1 }, { 1, 1 });
-  result->gui.sections[section_delay_monitor] = make_module_section_gui(section_delay_monitor, { 3, 1 }, { 1, 2 });
+  result->gui.sections[section_monitor] = make_module_section_gui(section_monitor, { 4, 1 }, { 1, 1 });
+  result->gui.sections[section_cv_matrix] = make_module_section_gui(section_cv_matrix, { 0, 1, 4, 1 }, { 1, 1 });
 
   result->modules.resize(module_count);
   result->modules[module_env] = env_topo(section_env, { 0, 0 });
   result->modules[module_osc] = osc_topo(section_osc, { 0, 0 });
+  result->modules[module_delay] = delay_topo(section_delay, { 0, 0 });
   result->modules[module_glfo] = lfo_topo(section_lfos, { 0, 0 }, true);
   result->modules[module_vlfo] = lfo_topo(section_lfos, { 0, 1 }, false);
-  result->modules[module_delay] = delay_topo(section_delay_monitor, { 0, 0 });
-  result->modules[module_monitor] = monitor_topo(section_delay_monitor, { 0, 1 }, result->polyphony);
+  result->modules[module_monitor] = monitor_topo(section_monitor, { 0, 0 }, result->polyphony);
   result->modules[module_filter] = filter_topo(section_filter, { 0, 0 }, result->modules[module_osc].info.slot_count);
   result->modules[module_cv_matrix] = cv_matrix_topo(section_cv_matrix, { 0, 0 },
     { &result->modules[module_glfo], &result->modules[module_vlfo], &result->modules[module_env] },
