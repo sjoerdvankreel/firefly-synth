0baa1dd5eceea47427d45755a9036417786313c6
sjoerdvankreel <sjoerdvankreel@gmail.com>
2023-10-29 14:35:22 +0100

    1063 retry cv matrix take 1

--- src/infernal_synth/infernal_synth/cv_matrix.cpp
+++ src/infernal_synth/infernal_synth/cv_matrix.cpp
@@ -17,25 +17,16 @@ namespace infernal_synth {
 static int constexpr route_count = 8;
 
 enum { section_main };
-enum { 
-  param_active, param_source, param_source_index, param_target, 
-  param_target_index, param_target_param, param_target_param_index };
+enum { param_active, param_source, param_target };
 
 class cv_matrix_engine:
 public module_engine { 
   cv_matrix_output _output = {};
-  std::vector<module_topo const*> const _sources;
-  std::vector<module_topo const*> const _targets;
-  std::vector<std::vector<param_topo>> _modulatable_params;
 public:
   void initialize() override {}
   void process(plugin_block& block) override;
 
-  cv_matrix_engine(
-    plugin_topo const& topo,
-    std::vector<module_topo const*> const& sources,
-    std::vector<module_topo const*> const& targets,
-    std::vector<std::vector<param_topo>> const& modulatable_params);
+  cv_matrix_engine(plugin_topo const& topo);
   INF_PREVENT_ACCIDENTAL_COPY_DEFAULT_CTOR(cv_matrix_engine);
 };
 
@@ -54,82 +45,57 @@ cv_matrix_topo(
 
   result.sections.emplace_back(make_param_section(section_main,
     make_topo_tag("{A19E18F8-115B-4EAB-A3C7-43381424E7AB}", "Main"), 
-    make_param_section_gui({ 0, 0 }, { { 1 }, { gui_dimension::auto_size, 2, 1, 2, 1, 2, 1 } })));
+    make_param_section_gui({ 0, 0 }, { { 1 }, { gui_dimension::auto_size, 1, 1 } })));
   
   result.params.emplace_back(make_param(
     make_topo_info("{4DF9B283-36FC-4500-ACE6-4AEBF74BA694}", "Active", param_active, route_count),
     make_param_dsp_block(param_automate::automate), make_domain_toggle(false),
     make_param_gui(section_main, gui_edit_type::toggle, param_layout::vertical, { 0, 0 }, make_label_none())));
   
+  std::vector<list_item> source_items;
+  for(int m = 0; m < sources.size(); m++)
+    for(int mi = 0; mi < sources[m]->info.slot_count; mi++)
+    {
+      list_item item;
+      auto const& tag = sources[m]->info.tag;
+      item.id = tag.id + "-" + std::to_string(mi);
+      item.name = tag.name + "-" + std::to_string(mi + 1);
+      source_items.push_back(item);
+    }
   auto& source = result.params.emplace_back(make_param(
     make_topo_info("{E6D638C0-2337-426D-8C8C-71E9E1595ED3}", "Source", param_source, route_count),
-    make_param_dsp_block(param_automate::none), make_domain_item(vector_map(sources, list_item::from_topo_ptr<module_topo>), ""),
+    make_param_dsp_block(param_automate::none), make_domain_item(source_items, ""),
     make_param_gui(section_main, gui_edit_type::list, param_layout::vertical, { 0, 1 }, make_label_none())));
   source.gui.bindings.enabled.bind({ param_active }, [](auto const& vs) { return vs[0] != 0; });
-  
-  auto map_to_slot_domain_ptr = [](auto const& t) { return make_domain_step(0, t->info.slot_count - 1, 1, 1); };
-  auto source_slot_domains = vector_map(sources, map_to_slot_domain_ptr);
-  auto& source_index = result.params.emplace_back(make_param(
-    make_topo_info("{5F6A54E9-50E6-4CDE-ACCB-4BA118F06780}", "Source Index", param_source_index, route_count),
-    make_param_dsp_block(param_automate::none), make_domain_dependent(source_slot_domains),
-    make_param_gui(section_main, gui_edit_type::dependent, param_layout::vertical, { 0, 2 }, make_label_none())));
-  source_index.gui.bindings.enabled.bind({ param_active }, [](auto const& vs) { return vs[0] != 0; });
-  source_index.dependent.bind({ param_source }, vector_explicit_copy(source_slot_domains), [](int const* vs) { return vs[0]; });
+
+  std::vector<list_item> target_items;
+  for(int m = 0; m < targets.size(); m++)
+    for (int mi = 0; mi < targets[m]->info.slot_count; mi++)
+      for(int p = 0; p < targets[m]->params.size(); p++)
+        if(targets[m]->params[p].dsp.automate == param_automate::modulate)
+          for (int pi = 0; pi < targets[m]->params[p].info.slot_count; pi++)
+          {
+            list_item item;
+            auto const& module_tag = targets[m]->info.tag;
+            auto const& param_tag = targets[m]->params[p].info.tag;
+            item.id = module_tag.id + "-" + std::to_string(mi) + "-" + param_tag.id + "-" + std::to_string(pi);
+            item.name = module_tag.name + "-" + std::to_string(mi + 1) + "-" + param_tag.name + "-" + std::to_string(pi + 1);
+            target_items.push_back(item);
+          }
 
   auto& target = result.params.emplace_back(make_param(
     make_topo_info("{94A037CE-F410-4463-8679-5660AFD1582E}", "Target", param_target, route_count),
-    make_param_dsp_block(param_automate::none), make_domain_item(vector_map(targets, list_item::from_topo_ptr<module_topo>), ""),
-    make_param_gui(section_main, gui_edit_type::list, param_layout::vertical, { 0, 3 }, make_label_none())));
+    make_param_dsp_block(param_automate::none), make_domain_item(target_items, ""),
+    make_param_gui(section_main, gui_edit_type::list, param_layout::vertical, { 0, 2 }, make_label_none())));
   target.gui.bindings.enabled.bind({ param_active }, [](auto const& vs) { return vs[0] != 0; });
-  
-  auto target_slot_domains = vector_map(targets, map_to_slot_domain_ptr);
-  auto& target_index = result.params.emplace_back(make_param(
-    make_topo_info("{79366858-994F-485F-BA1F-34AE3DFD2CEE}", "Target Index", param_target_index, route_count),
-    make_param_dsp_block(param_automate::none), make_domain_dependent(target_slot_domains),
-    make_param_gui(section_main, gui_edit_type::dependent, param_layout::vertical, { 0, 4 }, make_label_none())));
-  target_index.gui.bindings.enabled.bind({ param_active }, [](auto const& vs) { return vs[0] != 0; });
-  target_index.dependent.bind({ param_target }, vector_explicit_copy(target_slot_domains), [](int const* vs) { return vs[0]; });
 
-  std::vector<param_domain> target_param_domains;
-  auto is_modulatable = [](auto const& p) { return p.dsp.automate == param_automate::modulate; };
-  auto filter_modulatable = [is_modulatable](auto const& ps) { return vector_filter(ps, is_modulatable); };
-  auto map_params_to_items = [](auto const& ps) { return vector_map(ps, list_item::from_topo<param_topo>); };
-  auto map_items_to_domains = [](auto const& is) { return make_domain_item(is, ""); };
-  auto target_params = vector_map(targets, [](auto const& m) { return m->params; });
-  auto modulatable_target_params = vector_map(target_params, filter_modulatable);
-  auto modulatable_target_items = vector_map(modulatable_target_params, map_params_to_items);
-  auto modulatable_target_domains = vector_map(modulatable_target_items, map_items_to_domains);
-  auto& target_param = result.params.emplace_back(make_param(
-    make_topo_info("{EA395DC3-A357-4B76-BBC9-CE857FB9BC2A}", "Target Param", param_target_param, route_count),
-    make_param_dsp_block(param_automate::none), make_domain_dependent(modulatable_target_domains),
-    make_param_gui(section_main, gui_edit_type::dependent, param_layout::vertical, { 0, 5 }, make_label_none())));
-  target_param.gui.bindings.enabled.bind({ param_active }, [](auto const& vs) { return vs[0] != 0; });
-  target_param.dependent.bind({ param_target }, vector_explicit_copy(modulatable_target_domains), [](int const* vs) { return vs[0]; });
-
-  auto map_to_slot_domain = [](auto const& t) { return make_domain_step(0, t.info.slot_count - 1, 1, 1); };
-  auto modulatable_target_params_joined = vector_join(modulatable_target_params);
-  auto modulatable_target_param_index_domain_mappings = vector_index_count(modulatable_target_params);
-  auto modulatable_target_param_index_domains = vector_map(modulatable_target_params_joined, map_to_slot_domain);
-  auto& target_param_index = result.params.emplace_back(make_param(
-    make_topo_info("{05E7FB15-58AD-40EA-BA7F-FDAB255879ED}", "Target Param Index", param_target_param_index, route_count),
-    make_param_dsp_block(param_automate::none), make_domain_dependent(modulatable_target_param_index_domains),
-    make_param_gui(section_main, gui_edit_type::dependent, param_layout::vertical, { 0, 6 }, make_label_none())));
-  target_param_index.gui.bindings.enabled.bind({ param_active }, [](auto const& vs) { return vs[0] != 0; });
-  target_param_index.dependent.bind({ param_target, param_target_param }, vector_explicit_copy(modulatable_target_param_index_domains), 
-    [mappings = modulatable_target_param_index_domain_mappings](int const* vs) { return mappings[vs[0]][vs[1]]; });
-
-  result.engine_factory = [sources, targets, modulatable_target_params](auto const& topo, int, int) ->
-    std::unique_ptr<module_engine> { return std::make_unique<cv_matrix_engine>(topo, sources, targets, modulatable_target_params); };
+  result.engine_factory = [](auto const& topo, int, int) ->
+    std::unique_ptr<module_engine> { return std::make_unique<cv_matrix_engine>(topo); };
   return result;
 }
 
 cv_matrix_engine::
-cv_matrix_engine(
-  plugin_topo const& topo,
-  std::vector<module_topo const*> const& sources,
-  std::vector<module_topo const*> const& targets,
-  std::vector<std::vector<param_topo>> const& modulatable_params) :
-  _sources(sources), _targets(targets), _modulatable_params(modulatable_params)
+cv_matrix_engine(plugin_topo const& topo)
 {
   plugin_dims dims(topo);
   _output.modulation.resize(dims.module_slot_param_slot);
@@ -139,6 +105,7 @@ cv_matrix_engine(
 void
 cv_matrix_engine::process(plugin_block& block)
 {
+#if 0
   // set every modulatable parameter to its corresponding automation curve
   for (int tm = 0; tm < _targets.size(); tm++)
     for (int tmi = 0; tmi < _targets[tm]->info.slot_count; tmi++)
@@ -201,6 +168,7 @@ cv_matrix_engine::process(plugin_block& block)
   }
   
   *block.state.own_context = &_output;
+#endif
 }
 
 }
