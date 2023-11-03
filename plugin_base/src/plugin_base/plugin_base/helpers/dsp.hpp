d59b0c6b3607bf7a1bbde2333262819d70855392
sjoerdvankreel <sjoerdvankreel@gmail.com>
2023-11-03 19:22:56 +0100

    1137

--- plugin_base/src/plugin_base/plugin_base/helpers/dsp.hpp
+++ plugin_base/src/plugin_base/plugin_base/helpers/dsp.hpp
@@ -2,15 +2,19 @@
 
 #include <plugin_base/shared/jarray.hpp>
 #include <plugin_base/dsp/block/plugin.hpp>
-
 #include <vector>
 
 namespace plugin_base {
 
 jarray<float, 1> const&
 normalized_to_raw_into(
-  plugin_block const& block, int m, int p, 
-  jarray<float, 1> const& in, jarray<float, 1>& out);
+  plugin_block const& block, int m, int p,
+  jarray<float, 1> const& in, jarray<float, 1>& out)
+{
+  auto normalized_to_raw = [&block, m, p](float v) { return block.normalized_to_raw(m, p, v); };
+  in.transform_to(block.start_frame, block.end_frame, out, normalized_to_raw);
+  return out;
+}
 
 template <class TransformTimesig>
 jarray<float, 1> const&
@@ -21,7 +25,7 @@ sync_or_rate_into_scratch(
   auto& result = block.state.own_scratch[scratch];
   int sig_index = block.state.own_block_automation[timesig_p][0].step();
   timesig sig = block.plugin.modules[module].params[timesig_p].domain.timesigs[sig_index];
-  if (sync) std::fill(result.begin(), result.end(), transform_timesig(block.host.bpm, sig));
+  if (sync) result.fill(block.start_frame, block.end_frame, transform_timesig(block.host.bpm, sig));
   else normalized_to_raw_into(block, module, rate_p, block.state.own_accurate_automation[rate_p][0], result);
   return result;
 }
