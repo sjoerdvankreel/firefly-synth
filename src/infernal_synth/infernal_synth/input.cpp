71ef129664360586b7d1c575c946fd386dcf898b
sjoerdvankreel <sjoerdvankreel@gmail.com>
2023-11-08 15:49:02 +0100

    1236

--- src/infernal_synth/infernal_synth/input.cpp
+++ src/infernal_synth/infernal_synth/input.cpp
@@ -14,7 +14,7 @@ namespace infernal_synth {
 int aux_count = 3;
 
 enum { section_aux, section_midi };
-enum { param_aux, param_pb, param_mod, param_count };
+enum { param_aux, param_mod, param_pb, param_pb_range, param_count };
 
 class input_engine :
 public module_engine {
@@ -31,8 +31,8 @@ input_topo(int section, plugin_base::gui_colors const& colors, plugin_base::gui_
     make_topo_info("{E22B3B9D-2337-4DE5-AA34-EB3351948D6A}", "In", module_input, 1),
     make_module_dsp(module_stage::input, module_output::cv, 0, {
       make_topo_info("{9D36E713-80F9-49CA-9E81-17E424FF66EE}", "Aux", param_aux, aux_count),
-      make_topo_info("{EB8CBA31-212A-42EA-956E-69063BF93C58}", "PB", param_pb, 1),
-      make_topo_info("{91B915D6-0DCA-4F59-A396-6AF31DA28DBB}", "Mod", param_mod, 1) }),
+      make_topo_info("{91B915D6-0DCA-4F59-A396-6AF31DA28DBB}", "Mod", param_mod, 1),
+      make_topo_info("{EB8CBA31-212A-42EA-956E-69063BF93C58}", "PB", param_pb, 1) }),
     make_module_gui(section, colors, pos, { { 1 }, { 2, 1 } } )));
 
   result.engine_factory = [](auto const&, int, int) ->
@@ -50,19 +50,25 @@ input_topo(int section, plugin_base::gui_colors const& colors, plugin_base::gui_
 
   result.sections.emplace_back(make_param_section(section_midi,
     make_topo_tag("{56FD2FEB-3084-4E28-B56C-06D31406EB42}", "MIDI"),
-    make_param_section_gui({ 0, 1 }, gui_dimension({ 1 }, { 1, 1 }))));
+    make_param_section_gui({ 0, 1 }, gui_dimension({ 1 }, { 1, 1, 1 }))));
   
   result.params.emplace_back(make_param(
+    make_topo_info("{7696305C-28F3-4C54-A6CA-7C9DB5635153}", "Mod", param_mod, 1),
+    make_param_dsp_block(param_automate::modulate), make_domain_percentage(0, 1, 0, 0, true),
+    make_param_gui_single(section_midi, gui_edit_type::knob, { 0, 0 },
+      make_label(gui_label_contents::name, gui_label_align::left, gui_label_justify::center))));
+
+  result.params.emplace_back(make_param(
     make_topo_info("{D1B334A6-FA2F-4AE4-97A0-A28DD0C1B48D}", "PB", param_pb, 1),
-    make_param_dsp_accurate(param_automate::both), make_domain_percentage(-1, 1, 0, 0, true),
-    make_param_gui_single(section_midi, gui_edit_type::knob, { 0, 0 }, 
+    make_param_dsp_block(param_automate::modulate), make_domain_percentage(-1, 1, 0, 0, true),
+    make_param_gui_single(section_midi, gui_edit_type::knob, { 0, 1 }, 
     make_label(gui_label_contents::name, gui_label_align::left, gui_label_justify::center))));
 
   result.params.emplace_back(make_param(
-    make_topo_info("{7696305C-28F3-4C54-A6CA-7C9DB5635153}", "Mod", param_mod, 1),
-    make_param_dsp_accurate(param_automate::both), make_domain_percentage(0, 1, 0, 0, true),
-    make_param_gui_single(section_midi, gui_edit_type::knob, { 0, 1 },
-      make_label(gui_label_contents::name, gui_label_align::left, gui_label_justify::center)))); 
+    make_topo_info("{79B7592A-4911-4B04-8F71-5DD4B2733F4F}", "Range", param_pb_range, 1),
+    make_param_dsp_block(param_automate::none), make_domain_step(1, 24, 12, 0),
+    make_param_gui_single(section_midi, gui_edit_type::autofit_list, { 0, 2 },
+      make_label(gui_label_contents::name, gui_label_align::left, gui_label_justify::center))));
 
   return result;
 }
