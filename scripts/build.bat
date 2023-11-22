3d7f2a25d7ab95e5435f24ca2881c8319bdd1da7
sjoerdvankreel <sjoerdvankreel@gmail.com>
2023-11-22 16:20:11 +0100

    1387 rename take 1

--- scripts/build.bat
+++ scripts/build.bat
@@ -8,12 +8,12 @@ cmake ../..
 if %errorlevel% neq 0 exit /b !errorlevel!
 
 if "%1" == "1" goto nodebug
-msbuild /property:Configuration=Debug infernal_synth.sln
+msbuild /property:Configuration=Debug firefly_synth.sln
 if %errorlevel% neq 0 exit /b !errorlevel!
 
 :nodebug
 if "%1" == "0" goto norelease
-msbuild /property:Configuration=Release infernal_synth.sln
+msbuild /property:Configuration=Release firefly_synth.sln
 if %errorlevel% neq 0 exit /b !errorlevel!
 
 :norelease
