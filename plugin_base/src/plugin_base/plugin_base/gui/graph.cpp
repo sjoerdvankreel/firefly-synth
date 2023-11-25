3a4c6834a936ec4403b5d21545d91d816a9cdec6
sjoerdvankreel <sjoerdvankreel@gmail.com>
2023-11-25 15:54:19 +0100

    Revert "1425"
    This reverts commit 1ee5fed66c4822aeb63f514026fd93b97a3e8463.

--- plugin_base/src/plugin_base/plugin_base/gui/graph.cpp
+++ plugin_base/src/plugin_base/plugin_base/gui/graph.cpp
@@ -5,7 +5,7 @@ using namespace juce;
 namespace plugin_base {
 
 void 
-graph::paint(std::vector<float> const& data)
+graph::render(std::vector<float> const& data)
 {
   _data = data;
   for (int i = 0; i < _data.size(); i++)
@@ -42,13 +42,6 @@ graph::paint(Graphics& g)
   g.fillPath(p);
   g.setColour(_lnf->colors().graph_foreground);
   g.strokePath(pStroke, PathStrokeType(1));
-}
-
-void 
-plugin_graph::any_state_changed(int index, plain_value plain)
-{
-  if(should_render(index))
-    paint(render());
 }
 
 }
--- plugin_base/src/plugin_base/plugin_base/gui/graph.hpp
+++ plugin_base/src/plugin_base/plugin_base/gui/graph.hpp
@@ -1,14 +1,12 @@
 #pragma once
 
 #include <plugin_base/gui/lnf.hpp>
-#include <plugin_base/shared/state.hpp>
 
 #include <juce_gui_basics/juce_gui_basics.h>
 #include <vector>
 
 namespace plugin_base {
 
-// renders pretty images
 class graph:
 public juce::Component
 {
@@ -18,27 +16,7 @@ public juce::Component
 public:
   graph(lnf* lnf): _lnf(lnf) {}
   void paint(juce::Graphics& g) override;
-  void paint(std::vector<float> const& data);
-};
-
-// renders pretty images based on static plugin parameter state 
-// (i.e. based on ui values only, nothing from the audio thread)
-class plugin_graph:
-public graph,
-public any_state_listener
-{
-  plugin_state const* const _state;
-
-protected:
-  virtual std::vector<float> render() = 0;
-  virtual bool should_render(int index) = 0;
-  plugin_state const* const state() const { return _state; }
-
-public:
-  void any_state_changed(int index, plain_value plain);
-  ~plugin_graph() { _state->remove_any_listener(this); }
-  plugin_graph(lnf* lnf, plugin_state const* state): graph(lnf), _state(state)
-  { _state->add_any_listener(this); }
+  void render(std::vector<float> const& data);
 };
 
 }
