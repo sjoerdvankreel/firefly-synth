24c095092aa1b5762cde886b41cc3d75d9dfcf52
sjoerdvankreel <sjoerdvankreel@gmail.com>
2023-12-04 21:27:32 +0100

    1565

--- src/firefly_synth/firefly_synth/synth.cpp
+++ src/firefly_synth/firefly_synth/synth.cpp
@@ -130,7 +130,8 @@ make_cv_matrix_sources(plugin_topo const* topo, bool global)
   if(global)
     return { &topo->modules[module_midi], &topo->modules[module_master_cv], &topo->modules[module_glfo] };
   else
-    return { &topo->modules[module_midi], &topo->modules[module_master_cv], &topo->modules[module_glfo], &topo->modules[module_vlfo], &topo->modules[module_env] };
+    return { &topo->modules[module_midi], &topo->modules[module_master_cv], &topo->modules[module_voice_cv], 
+      &topo->modules[module_glfo], &topo->modules[module_vlfo], &topo->modules[module_env] };
 }
 
 std::unique_ptr<tab_menu_handler>
@@ -206,6 +207,7 @@ synth_topo()
 
   result->modules.resize(module_count);
   result->modules[module_midi] = midi_topo(module_section_hidden);
+  result->modules[module_voice_cv] = voice_cv_topo(module_section_hidden);
   result->modules[module_voice_audio_in] = voice_audio_in_topo(module_section_hidden);
   result->modules[module_voice_audio_out] = voice_audio_out_topo(module_section_hidden);
   result->modules[module_env] = env_topo(module_section_env, cv_colors, { 0, 0 });
---
+++ src/firefly_synth/firefly_synth/voice_cv.cpp
@@ -0,0 +1,45 @@
+#include <plugin_base/helpers/dsp.hpp>
+#include <plugin_base/topo/plugin.hpp>
+#include <plugin_base/topo/support.hpp>
+#include <plugin_base/shared/state.hpp>
+#include <plugin_base/dsp/engine.hpp>
+#include <plugin_base/dsp/utility.hpp>
+
+#include <firefly_synth/synth.hpp>
+#include <cmath>
+
+using namespace plugin_base;
+
+namespace firefly_synth {
+
+enum { output_key, output_velo, output_count };
+
+class voice_cv_engine :
+public module_engine {
+public:
+  void initialize() override { }
+  void process(plugin_block& block) override;
+  PB_PREVENT_ACCIDENTAL_COPY_DEFAULT_CTOR(voice_cv_engine);
+};
+
+module_topo
+voice_cv_topo(int section)
+{
+  module_topo result(make_module(
+    make_topo_info("{4380584E-6CC5-4DA5-A533-17A9A1777476}", "Voice", module_voice_cv, 1),
+    make_module_dsp(module_stage::voice, module_output::cv, 0, {
+      make_module_dsp_output(true, make_topo_info("{376846A2-33FC-4DB0-BCB9-7A43A8488A7F}", "Key", output_key, 1)),
+      make_module_dsp_output(true, make_topo_info("{2D59B6B8-3B08-430C-9A8A-E882C8E14597}", "Velo", output_velo, 1)) }),
+      make_module_gui_none(section)));
+
+  result.engine_factory = [](auto const&, int, int) ->
+    std::unique_ptr<module_engine> { return std::make_unique<voice_cv_engine>(); };
+  return result;
+}
+
+void
+voice_cv_engine::process(plugin_block& block)
+{
+}
+
+}
