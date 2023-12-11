54538b32c419e499db03fd1fecdec83308d17a4c
sjoerdvankreel <sjoerdvankreel@gmail.com>
2023-12-11 19:21:48 +0100

    Revert "Revert "1670""
    This reverts commit f98cd7446ed682b5144c2e743dae59c2cc21ab26.

--- src/firefly_synth/firefly_synth/synth.cpp
+++ src/firefly_synth/firefly_synth/synth.cpp
@@ -22,8 +22,8 @@ enum {
 enum { 
   module_section_hidden, module_section_master_in, module_section_voice_in, 
   module_section_g_lfo_fx, module_section_env, module_section_cv_matrix,
-  module_section_audio_matrix, module_section_osc, module_section_v_lfo_fx,
-  module_section_monitor_out, module_section_count };
+  module_section_audio_matrix, module_section_osc, module_section_v_lfo_monitor,
+  module_section_out_v_fx, module_section_count };
 
 static gui_colors
 make_section_colors(Colour const& c)
@@ -215,14 +215,14 @@ synth_topo()
     "{AB26F56E-DC6D-4F0B-845D-C750728F8FA2}", module_section_env, { 4, 0, 1, 3 }, { 1, 1 });
   result->gui.module_sections[module_section_osc] = make_module_section_gui(
     "{7A457CCC-E719-4C07-98B1-017EA7DEFB1F}", module_section_osc, { 6, 0, 1, 3 }, { 1, 1 });
-  result->gui.module_sections[module_section_v_lfo_fx] = make_module_section_gui(
-    "{96C75EE5-577E-4508-A85A-E92FF9FD8A4D}", module_section_v_lfo_fx, { 3, 0, 1, 3 }, { 1, 2 });
+  result->gui.module_sections[module_section_v_lfo_monitor] = make_module_section_gui(
+    "{96C75EE5-577E-4508-A85A-E92FF9FD8A4D}", module_section_v_lfo_monitor, { 3, 0, 1, 3 }, { 1, 2 });
   result->gui.module_sections[module_section_master_in] = make_module_section_gui(
     "{F9578AAA-66A4-4B0C-A941-4719B5F0E998}", module_section_master_in, { 1, 0, 1, 3 }, { 1, 1 });
   result->gui.module_sections[module_section_voice_in] = make_module_section_gui(
     "{FB435C64-8349-4F0F-84FC-FFC82002D69F}", module_section_voice_in, { 5, 0, 1, 3 }, { 1, 1 });
-  result->gui.module_sections[module_section_monitor_out] = make_module_section_gui(
-    "{8FDAEB21-8876-4A90-A8E1-95A96FB98FD8}", module_section_monitor_out, { 7, 0, 1, 3 }, { { 1 }, { 1, 1, 2 } });
+  result->gui.module_sections[module_section_out_v_fx] = make_module_section_gui(
+    "{8FDAEB21-8876-4A90-A8E1-95A96FB98FD8}", module_section_out_v_fx, { 7, 0, 1, 3 }, { { 1 }, { 1, 1, 2 } });
   result->gui.module_sections[module_section_cv_matrix] = make_module_section_gui_tabbed(
     "{11A46FE6-9009-4C17-B177-467243E171C8}", module_section_cv_matrix, { 1, 3, 4, 2 },
     "CV", result->gui.module_header_width, { module_vcv_matrix, module_gcv_matrix });
@@ -237,15 +237,15 @@ synth_topo()
   result->modules[module_env] = env_topo(module_section_env, voice_colors, { 0, 0 });
   result->modules[module_osc] = osc_topo(module_section_osc, voice_colors, { 0, 0 });
   result->modules[module_gfx] = fx_topo(module_section_g_lfo_fx, global_colors, { 0, 1 }, true);
-  result->modules[module_vfx] = fx_topo(module_section_v_lfo_fx, voice_colors, { 0, 1 }, false);
+  result->modules[module_vfx] = fx_topo(module_section_out_v_fx, voice_colors, { 0, 2 }, false);
   result->modules[module_glfo] = lfo_topo(module_section_g_lfo_fx, global_colors, { 0, 0 }, true);
-  result->modules[module_vlfo] = lfo_topo(module_section_v_lfo_fx, voice_colors, { 0, 0 }, false);
+  result->modules[module_vlfo] = lfo_topo(module_section_v_lfo_monitor, voice_colors, { 0, 0 }, false);
   result->modules[module_master_in] = master_in_topo(module_section_master_in, global_colors, { 0, 0 });
   result->modules[module_voice_on_note] = voice_on_note_topo(result.get(), module_section_hidden); // must be after all global cv  
   result->modules[module_voice_in] = voice_in_topo(module_section_voice_in, voice_colors, { 0, 0 }); // must be after all cv
-  result->modules[module_voice_out] = audio_out_topo(module_section_monitor_out, voice_colors, { 0, 0 }, false);
-  result->modules[module_master_out] = audio_out_topo(module_section_monitor_out, global_colors, { 0, 1 }, true);
-  result->modules[module_monitor] = monitor_topo(module_section_monitor_out, monitor_colors, { 0, 2 }, result->polyphony);
+  result->modules[module_voice_out] = audio_out_topo(module_section_out_v_fx, voice_colors, { 0, 0 }, false);
+  result->modules[module_master_out] = audio_out_topo(module_section_out_v_fx, global_colors, { 0, 1 }, true);
+  result->modules[module_monitor] = monitor_topo(module_section_v_lfo_monitor, monitor_colors, { 0, 1 }, result->polyphony);
   result->modules[module_gaudio_matrix] = audio_matrix_topo(module_section_audio_matrix, matrix_colors, { 0, 0 }, true,
     make_audio_matrix_sources(result.get(), true), make_audio_matrix_targets(result.get(), true));
   result->modules[module_vaudio_matrix] = audio_matrix_topo(module_section_audio_matrix, matrix_colors, { 0, 0 }, false,
