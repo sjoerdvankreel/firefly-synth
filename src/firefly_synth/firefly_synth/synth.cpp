95d850f182e181ca73f2b7c4dd43e8fd954afd24
sjoerdvankreel <sjoerdvankreel@gmail.com>
2023-12-18 19:54:56 +0100

    1831

--- src/firefly_synth/firefly_synth/synth.cpp
+++ src/firefly_synth/firefly_synth/synth.cpp
@@ -201,7 +201,7 @@ synth_topo()
   result->gui.custom_sections[custom_section_title] = make_custom_section_gui(
     custom_section_title, { 0, 0, 1, 1 }, custom_colors, make_title_section_ui);
   result->gui.custom_sections[custom_section_controls] = make_custom_section_gui(
-    custom_section_controls, { 0, 3, 1, 2 }, custom_colors, make_controls_section);
+    custom_section_controls, { 1, 3, 1, 2 }, custom_colors, make_controls_section);
   result->gui.custom_sections[custom_section_hover_graph] = make_custom_section_gui(
     custom_section_hover_graph, { 0, 1, 1, 1 }, custom_colors, [](auto* gui, auto* lnf, auto store)
     -> Component& { return make_graph_section(gui, lnf, store, true); });
@@ -213,23 +213,23 @@ synth_topo()
   result->gui.module_sections[module_section_hidden] = make_module_section_gui_none(
     "{F289D07F-0A00-4AB1-B87B-685CB4D8B2F8}", module_section_hidden);
   result->gui.module_sections[module_section_vlfo] = make_module_section_gui(
-    "{0DA0E7C3-8DBB-440E-8830-3B6087F23B81}", module_section_vlfo, { 6, 0, 1, 3 }, { 1, 1 });
+    "{0DA0E7C3-8DBB-440E-8830-3B6087F23B81}", module_section_vlfo, { 7, 0, 1, 3 }, { 1, 1 });
   result->gui.module_sections[module_section_vfx] = make_module_section_gui(
-    "{5BD5F320-3CA9-490B-B69E-36A003F41CEC}", module_section_vfx, { 7, 0, 1, 3 }, { 1, 1 });
+    "{5BD5F320-3CA9-490B-B69E-36A003F41CEC}", module_section_vfx, { 6, 0, 1, 3 }, { 1, 1 });
   result->gui.module_sections[module_section_env] = make_module_section_gui(
     "{AB26F56E-DC6D-4F0B-845D-C750728F8FA2}", module_section_env, { 8, 0, 1, 3 }, { 1, 1 });
   result->gui.module_sections[module_section_osc_voice_out] = make_module_section_gui(
     "{7A457CCC-E719-4C07-98B1-017EA7DEFB1F}", module_section_osc_voice_out, { 5, 0, 1, 3 }, { { 1 }, { 3, 1 } });
   result->gui.module_sections[module_section_glfo] = make_module_section_gui(
-    "{96C75EE5-577E-4508-A85A-E92FF9FD8A4D}", module_section_glfo, { 2, 0, 1, 3 }, { 1, 1 });
+    "{96C75EE5-577E-4508-A85A-E92FF9FD8A4D}", module_section_glfo, { 3, 0, 1, 3 }, { 1, 1 });
   result->gui.module_sections[module_section_gfx] = make_module_section_gui(
-    "{654B206B-27AE-4DFD-B885-772A8AD0A4F3}", module_section_gfx, { 3, 0, 1, 3 }, { 1, 1 });
+    "{654B206B-27AE-4DFD-B885-772A8AD0A4F3}", module_section_gfx, { 2, 0, 1, 3 }, { 1, 1 });
   result->gui.module_sections[module_section_master_in_out] = make_module_section_gui(
     "{F9578AAA-66A4-4B0C-A941-4719B5F0E998}", module_section_master_in_out, { 1, 0, 1, 3 }, { { 1 }, { 3, 1 } });
   result->gui.module_sections[module_section_voice_in] = make_module_section_gui(
     "{FB435C64-8349-4F0F-84FC-FFC82002D69F}", module_section_voice_in, { 4, 0, 1, 3 }, { 1, 1 });
   result->gui.module_sections[module_section_monitor] = make_module_section_gui(
-    "{8FDAEB21-8876-4A90-A8E1-95A96FB98FD8}", module_section_monitor, { 1, 3, 1, 2 }, { { 1 }, { 1 } });
+    "{8FDAEB21-8876-4A90-A8E1-95A96FB98FD8}", module_section_monitor, { 0, 3, 1, 2 }, { { 1 }, { 1 } });
   result->gui.module_sections[module_section_matrices] = make_module_section_gui_tabbed(
     "{11A46FE6-9009-4C17-B177-467243E171C8}", module_section_matrices, { 2, 3, 7, 2 },
     { module_am_matrix, module_vaudio_matrix, module_gaudio_matrix, module_vcv_matrix, module_gcv_matrix });
