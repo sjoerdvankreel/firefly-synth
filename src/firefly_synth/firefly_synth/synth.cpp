be807bcbe5ca2734fd9469a4a388c21f18262582
sjoerdvankreel <sjoerdvankreel@gmail.com>
2023-12-18 20:46:26 +0100

    1835

--- src/firefly_synth/firefly_synth/synth.cpp
+++ src/firefly_synth/firefly_synth/synth.cpp
@@ -57,14 +57,14 @@ make_graph_section(plugin_gui* gui, lnf* lnf, component_store store, bool render
 static Component&
 make_controls_section(plugin_gui* gui, lnf* lnf, component_store store)
 {
-  auto& result = store_component<grid_component>(store, gui_dimension { 2, 4 }, 2);
+  auto& result = store_component<grid_component>(store, gui_dimension { 1, 7 }, 2);
   result.add(gui->make_load_button(), {0, 0});
   result.add(gui->make_save_button(), {0, 1});
   result.add(gui->make_init_button(), {0, 2});
   result.add(gui->make_clear_button(), {0, 3});
-  result.add(store_component<preset_button>(store, gui), { 1, 3 });
-  result.add(store_component<last_tweaked_label>(store, gui->gui_state(), "Tweak:"), {1, 0, 1, 2});
-  result.add(store_component<last_tweaked_editor>(store, gui->gui_state(), lnf), { 1, 2 });
+  result.add(store_component<preset_button>(store, gui), { 0, 4 });
+  result.add(store_component<last_tweaked_label>(store, gui->gui_state(), "Tweak:"), { 0, 5 });
+  result.add(store_component<last_tweaked_editor>(store, gui->gui_state(), lnf), { 0, 6 });
   return result;
 }
 
