4d1dfb2dd42aacb3bc814ca44e819aaecea6ac3d
sjoerdvankreel <sjoerdvankreel@gmail.com>
2023-12-27 02:13:23 +0100

    Revert "2021 tryout"
    This reverts commit dae186d3929f1d3ce01f2dcd05706ce99794e821.

--- src/firefly_synth/firefly_synth/oscillator.cpp
+++ src/firefly_synth/firefly_synth/oscillator.cpp
@@ -203,11 +203,11 @@ osc_engine::process(plugin_block& block, cv_matrix_mixdown const* modulation, ja
   float sample;
   int note = block_auto[param_note][0].step();
   auto const& cent_curve = *(*modulation)[module_osc][block.module_slot][param_cent][0];
-  //auto const& voice_pitch_offset_curve = block.voice->all_cv[module_voice_in][0][voice_in_output_pitch_offset][0];
+  auto const& voice_pitch_offset_curve = block.voice->all_cv[module_voice_in][0][voice_in_output_pitch_offset][0];
   for (int f = block.start_frame; f < block.end_frame; f++)
   {
     float cent = block.normalized_to_raw(module_osc, param_cent, cent_curve[f]);
-    float freq = pitch_to_freq(note + cent);
+    float freq = pitch_to_freq(note + cent + voice_pitch_offset_curve[f]);
     float inc = std::clamp(freq, 0.0f, block.sample_rate * 0.5f) / block.sample_rate;
     switch (type)
     {
