02296c74cee4c7b4682fd736bb9c1288da05247d
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-01-02 22:44:47 +0100

    2172

--- src/firefly_synth/firefly_synth/fx.cpp
+++ src/firefly_synth/firefly_synth/fx.cpp
@@ -213,7 +213,7 @@ fx_topo(int section, gui_colors const& colors, gui_position const& pos, bool glo
       make_label(gui_label_contents::name, gui_label_align::left, gui_label_justify::center))));
   auto& svf_gain = result.params.emplace_back(make_param(
     make_topo_info("{FE108A32-770A-415B-9C85-449ABF6A944C}", "Gain", param_svf_gain, 1),
-    make_param_dsp_accurate(param_automate::automate_modulate), make_domain_percentage(-8, 8, 0, 0, true),
+    make_param_dsp_accurate(param_automate::automate_modulate), make_domain_linear(-24, 24, 0, 1, "dB"),
     make_param_gui_single(section_svf, gui_edit_type::knob, { 0, 3 }, gui_label_contents::value,
       make_label(gui_label_contents::name, gui_label_align::left, gui_label_justify::center))));
   svf_gain.gui.bindings.enabled.bind_params({ param_type }, [](auto const& vs) { return is_svf_gain(vs[0]); });
@@ -265,10 +265,10 @@ fx_topo(int section, gui_colors const& colors, gui_position const& pos, bool glo
 
 static void
 init_svf(
-  double w, double res, double g, 
+  double w, double res, double g, double a,
   double& k, double& a1, double& a2, double& a3)
 {
-  k = 2 - 2 * res;
+  k = (2 - 2 * res) / a;
   a1 = 1 / (1 + g * (g + k));
   a2 = g * a1;
   a3 = g * a2;
@@ -276,68 +276,80 @@ init_svf(
 
 static void
 init_svf_lpf(
-  double w, double res,
+  double w, double res, double db_gain,
   double& a1, double& a2, double& a3, 
   double& m0, double& m1, double& m2)
 {
   double k;
-  init_svf(w, res, std::tan(w), k, a1, a2, a3);
+  init_svf(w, res, std::tan(w), 1, k, a1, a2, a3);
   m0 = 0; m1 = 0; m2 = 1;
 }
 
 static void
 init_svf_hpf(
-  double w, double res,
+  double w, double res, double db_gain,
   double& a1, double& a2, double& a3, 
   double& m0, double& m1, double& m2)
 {
   double k;
-  init_svf(w, res, std::tan(w), k, a1, a2, a3);
+  init_svf(w, res, std::tan(w), 1, k, a1, a2, a3);
   m0 = 1; m1 = -k; m2 = -1;
 }
 
 static void
 init_svf_bpf(
-  double w, double res,
+  double w, double res, double db_gain,
   double& a1, double& a2, double& a3,
   double& m0, double& m1, double& m2)
 {
   double k;
-  init_svf(w, res, std::tan(w), k, a1, a2, a3);
+  init_svf(w, res, std::tan(w), 1, k, a1, a2, a3);
   m0 = 0; m1 = 1; m2 = 0;
 }
 
 static void
 init_svf_bsf(
-  double w, double res,
+  double w, double res, double db_gain,
   double& a1, double& a2, double& a3,
   double& m0, double& m1, double& m2)
 {
   double k;
-  init_svf(w, res, std::tan(w), k, a1, a2, a3);
+  init_svf(w, res, std::tan(w), 1, k, a1, a2, a3);
   m0 = 1; m1 = -k; m2 = 0;
 }
 
 static void
 init_svf_apf(
-  double w, double res,
+  double w, double res, double db_gain,
   double& a1, double& a2, double& a3,
   double& m0, double& m1, double& m2)
 {
   double k;
-  init_svf(w, res, std::tan(w), k, a1, a2, a3);
+  init_svf(w, res, std::tan(w), 1, k, a1, a2, a3);
   m0 = 1; m1 = -2 * k; m2 = 0;
 }
 
 static void
 init_svf_peq(
-  double w, double res,
+  double w, double res, double db_gain,
   double& a1, double& a2, double& a3,
   double& m0, double& m1, double& m2)
 {
   double k;
-  init_svf(w, res, std::tan(w), k, a1, a2, a3);
+  init_svf(w, res, std::tan(w), 1, k, a1, a2, a3);
   m0 = 1; m1 = -k; m2 = -2;
+}
+
+static void
+init_svf_bll(
+  double w, double res, double db_gain,
+  double& a1, double& a2, double& a3,
+  double& m0, double& m1, double& m2)
+{
+  double k;
+  double a = std::pow(10.0, db_gain / 40.0);
+  init_svf(w, res, std::tan(w), a, k, a1, a2, a3);
+  m0 = 1; m1 = k * (a - 1); m2 = 0;
 }
 
 fx_engine::
@@ -384,6 +396,7 @@ fx_engine::process(plugin_block& block,
   case type_svf_bsf: process_svf(block, *modulation, init_svf_bsf); break;
   case type_svf_peq: process_svf(block, *modulation, init_svf_peq); break;
   case type_svf_apf: process_svf(block, *modulation, init_svf_apf); break;
+  case type_svf_bll: process_svf(block, *modulation, init_svf_bll); break;
   }
 }
 
@@ -411,20 +424,22 @@ fx_engine::process_delay(plugin_block& block, cv_matrix_mixdown const& modulatio
 template <class Init> void
 fx_engine::process_svf(plugin_block& block, cv_matrix_mixdown const& modulation, Init init)
 {
-  double w, hz;
   double a1, a2, a3;
   double m0, m1, m2;
+  double w, hz, gain;
 
   double const max_res = 0.99;
   int this_module = _global ? module_gfx : module_vfx;
   auto const& res_curve = *modulation[this_module][block.module_slot][param_svf_res][0];
   auto const& freq_curve = *modulation[this_module][block.module_slot][param_svf_freq][0];
+  auto const& gain_curve = *modulation[this_module][block.module_slot][param_svf_gain][0];
 
   for (int f = block.start_frame; f < block.end_frame; f++)
   {
     hz = block.normalized_to_raw(this_module, param_svf_freq, freq_curve[f]);
+    gain = block.normalized_to_raw(this_module, param_svf_freq, gain_curve[f]);
     w = pi64 * hz / block.sample_rate;
-    init(w, res_curve[f] * max_res, a1, a2, a3, m0, m1, m2);
+    init(w, res_curve[f] * max_res, gain, a1, a2, a3, m0, m1, m2);
     for (int c = 0; c < 2; c++)
     {
       double v0 = block.state.own_audio[0][0][c][f];
