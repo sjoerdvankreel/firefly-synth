d5416e38ad0c8278d43f40fd2fb76dbef5d362a6
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-01-27 12:18:33 +0100

    2771

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,15 +4,7 @@ set -e
 cd ..
 mkdir -p build/linux/debug
 cd build/linux/debug
-cmake -DCMAKE_BUILD_TYPE=Debug ../../..
+cmake -DCMAKE_BUILD_TYPE="$1" ../../..
 make
-
-if [ "$1" -eq "1" ] ; then
-cd ..
-mkdir -p release
-cd release
-cmake -DCMAKE_BUILD_TYPE=Release ../../..
-make
-fi
 
 cd ../../../scripts
