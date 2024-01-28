70a4f38555170cd449a95ef3841b4e954fe80ee7
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-01-28 21:48:59 +0100

    take 2

--- plugin_base/src/plugin_base.param_ref_gen/plugin_base.param_ref_gen/param_ref_gen.cpp
+++ plugin_base/src/plugin_base.param_ref_gen/plugin_base.param_ref_gen/param_ref_gen.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 #include <plugin_base/topo/plugin.hpp>
 
+static int 
+fail(std::string const& text)
+{
+  std::cout << text << std::endl;
+  return 1;
+}
+
 int 
 main(int argc, char** argv)
 {
+  if(argc != 3) 
+    return fail("Usage: param_ref_gen path_to_library path_to_output.html.");
   return 0;
 }
