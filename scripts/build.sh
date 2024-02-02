bd56458f443d3450f0288d9b68bc4450f159317b
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-02-02 20:12:28 +0100

    take 65

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,4 +7,6 @@ cd build/linux/"$1"
 cmake -DCMAKE_BUILD_TYPE="$1" ../../..
 make
 
-cd ../../../scripts
+cd ../../dist/"%1"/linux
+plugin_base.ref_gen firefly_synth_1.vst3/Contents/x86_64-linux/firefly_synth_1.vst3 ../../../PARAMREF.html
+cd ../../../../scripts
