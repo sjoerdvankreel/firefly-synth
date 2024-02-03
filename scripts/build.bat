85e6dbe0d339b8027fc14a1c6f2a0e8176a5a9d6
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-02-03 18:26:34 +0100

    manual

--- scripts/build.bat
+++ scripts/build.bat
@@ -11,7 +11,7 @@ msbuild /property:Configuration="%1" firefly_synth.sln
 if %errorlevel% neq 0 exit /b !errorlevel!
 
 cd ..\..\dist\"%1"\win
-plugin_base.ref_gen.exe firefly_synth_1.vst3\Contents\x86_64-win\firefly_synth_1.vst3 ..\..\..\PARAMREF.html
+plugin_base.ref_gen.exe firefly_synth_1.vst3\Contents\x86_64-win\firefly_synth_1.vst3 ..\..\..\param_reference.html
 if %errorlevel% neq 0 exit /b !errorlevel!
 
 cd ..\..\..\..\scripts
