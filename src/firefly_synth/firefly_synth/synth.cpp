1f8ae7e1cf8ce57f954ffe35ad64c0482e3220b1
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-02-28 00:41:19 +0100

    fix width

--- src/firefly_synth/firefly_synth/synth.cpp
+++ src/firefly_synth/firefly_synth/synth.cpp
@@ -324,7 +324,7 @@ synth_topo(bool is_fx)
 #endif
 
   result->gui.typeface_file_name = "Handel Gothic Regular.ttf";
-  result->gui.dimension.column_sizes = { is_fx? 19: 17, is_fx? 28: 30, 9, 31 };
+  result->gui.dimension.column_sizes = { is_fx? 19: 17, is_fx? 28: 30, 7, 26 };
   int height = result->gui.min_width * result->gui.aspect_ratio_height / result->gui.aspect_ratio_width;
   std::vector<gui_vertical_section_size> section_vsizes = { { true, 1 }, { !is_fx, 1 }, { true, 1 }, { true, 1 }, { true, 1 } };
   if (!is_fx) section_vsizes.insert(section_vsizes.end(), { { true, 2 }, { true, 1 }, { true, 1 }, { true, 2 } });
