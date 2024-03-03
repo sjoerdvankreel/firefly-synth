a50fa926f3297df37d5c369b8be37af1a038d14e
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-03-03 21:05:33 +0100

    better labels

--- src/firefly_synth/firefly_synth/voice_in.cpp
+++ src/firefly_synth/firefly_synth/voice_in.cpp
@@ -93,7 +93,7 @@ voice_in_topo(int section, gui_colors const& colors, gui_position const& pos)
     make_topo_info("{524138DF-1303-4961-915A-3CAABA69D53A}", true, "Voice In", "Voice In", "VIn", module_voice_in, 1),
     make_module_dsp(module_stage::voice, module_output::cv, 0, {
       make_module_dsp_output(false, make_topo_info_basic("{58E73C3A-CACD-48CC-A2B6-25861EC7C828}", "Pitch", 0, 1)) }),
-    make_module_gui(section, colors, pos, { { 1 }, { 5, gui_dimension::auto_size, 3 } } )));
+    make_module_gui(section, colors, pos, { { 1 }, { 5, gui_dimension::auto_size, 4 } } )));
   result.info.description = "Oscillator common module. Controls portamento, oversampling and base pitch for all oscillators.";
   
   result.graph_renderer = render_graph;
