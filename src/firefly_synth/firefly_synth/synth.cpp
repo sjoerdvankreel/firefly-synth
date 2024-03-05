bca87caf213fb9b7723889a35e2743d151f95dbd
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-03-05 23:18:14 +0100

    better

--- src/firefly_synth/firefly_synth/synth.cpp
+++ src/firefly_synth/firefly_synth/synth.cpp
@@ -150,9 +150,12 @@ static Component&
 make_edit_controls_section(plugin_gui* gui, lnf* lnf, bool is_fx, component_store store)
 {
   auto& result = store_component<grid_component>(store, gui_dimension{ 2, 2 }, 2);
-  auto& tweak_label = store_component<last_tweaked_label>(store, gui->gui_state());
-  tweak_label.setJustificationType(Justification::centredRight);
-  result.add(tweak_label, { 0, 0, 1, 2 });
+  auto& tweak_label = store_component<juce::Label>(store);
+  tweak_label.setText("Last Tweaked", juce::dontSendNotification);
+  result.add(tweak_label, { 0, 0, 1, 1 });
+  auto& tweak = store_component<last_tweaked_label>(store, gui->gui_state());
+  tweak.setJustificationType(Justification::centredRight);
+  result.add(tweak, { 0, 1, 1, 1 });
   result.add(store_component<last_tweaked_editor>(store, gui->gui_state(), lnf), { 1, 0 });
   result.add(store_component<preset_button>(store, gui), { 1, 1 });
   return result;
