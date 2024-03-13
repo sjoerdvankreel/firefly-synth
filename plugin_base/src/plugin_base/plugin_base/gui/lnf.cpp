82500818132f83c57de9f80990bf3d5419452319
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-03-13 12:18:49 +0100

    50

--- plugin_base/src/plugin_base/plugin_base/gui/lnf.cpp
+++ plugin_base/src/plugin_base/plugin_base/gui/lnf.cpp
@@ -211,6 +211,12 @@ lnf::init_theme(std::filesystem::path const& theme_folder, var const& json)
     _theme_settings.lighten = (float)settings["lighten"];
   if (settings.hasProperty("font_height"))
     _theme_settings.font_height = (float)settings["font_height"];
+  if (settings.hasProperty("table_cell_radius"))
+    _theme_settings.table_cell_radius = (float)settings["table_cell_radius"];
+  if (settings.hasProperty("text_editor_radius"))
+    _theme_settings.text_editor_radius = (float)settings["text_editor_radius"];
+  if (settings.hasProperty("combo_radius"))
+    _theme_settings.combo_radius = (float)settings["combo_radius"];
   if (settings.hasProperty("module_tab_width"))
     _theme_settings.module_tab_width = (int)settings["module_tab_width"];
   if (settings.hasProperty("module_header_width"))
