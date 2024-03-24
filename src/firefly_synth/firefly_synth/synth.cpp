5801f7fd7fcad9befe7c8f2c1d2f507dc47f35d4
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-03-24 19:18:10 +0100

    take 2

--- src/firefly_synth/firefly_synth/synth.cpp
+++ src/firefly_synth/firefly_synth/synth.cpp
@@ -209,10 +209,11 @@ make_title_section(plugin_gui* gui, lnf* lnf, component_store store, bool is_fx)
   grid.add(store_component<image_component>(store, gui->gui_state()->desc().config, lnf->theme(), "header.png", RectanglePlacement::xRight), { 0, 1, 2, 1 });
   auto& title_label = store_component<autofit_label>(store, lnf, name, true, 15);
   title_label.setColour(Label::ColourIds::textColourId, colors.control_text);
+  title_label.setJustificationType(Justification::centredLeft);
   grid.add(title_label, { 0, 0, 1, 1 });
   std::string version_text = std::string(FF_SYNTH_VERSION_TEXT) + " " + gui->gui_state()->desc().config->format_name();
   auto& version_label = store_component<autofit_label>(store, lnf, version_text, false, 10);
-  version_label.setJustificationType(Justification::centredRight);
+  version_label.setJustificationType(Justification::centredLeft);
   version_label.setColour(Label::ColourIds::textColourId, colors.control_text);
   grid.add(version_label, { 1, 0, 1, 1 });
   return grid;
