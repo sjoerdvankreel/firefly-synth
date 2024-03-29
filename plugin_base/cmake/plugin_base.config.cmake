68b0f9e4c7fd0d3f6c8424c87a17b6161ad434bb
sjoerdvankreel@gmail.com <sjoerdvankreel@gmail.com>
2024-03-29 21:56:36 +0100

    take 4

--- plugin_base/cmake/plugin_base.config.cmake
+++ plugin_base/cmake/plugin_base.config.cmake
@@ -2,7 +2,7 @@ cmake_policy(SET CMP0091 NEW)
 
 if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
 add_compile_options(/W4;/WX;/external:W0;/wd4245;/wd4267;/wd4244;/wd4100;/wd26495;/wd4389;/fp:fast;/arch:AVX)
-elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+elseif((CMAKE_CXX_COMPILER_ID STREQUAL "GNU") OR (CMAKE_CXX_COMPILER_ID MATCHES "Clang")) 
 add_compile_options(-Wl,--no-undefined -fvisibility=hidden -fvisibility-inlines-hidden)
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wl,--no-undefined -fvisibility=hidden -fvisibility-inlines-hidden")
 else()
