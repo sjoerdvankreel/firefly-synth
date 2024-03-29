3c903c3f6bdeee523654e63f08358faa2f84fc01
sjoerdvankreel@gmail.com <sjoerdvankreel@gmail.com>
2024-03-30 00:35:19 +0100

    take 16

--- scripts/build_mac.sh
+++ scripts/build_mac.sh
@@ -2,8 +2,12 @@
 set -e
 
 cd ..
-mkdir -p build/mac
-cd build/mac
-cmake -G Xcode ../..
-Xcodebuild -configuration "$1"
- 
+mkdir -p build/mac/"$1"
+cd build/mac/"$1"
+export XCODE_VERSION="15" 
+cmake -DCMAKE_BUILD_TYPE="$1" -DXCODE_VERSION="15" ../../..
+make
+
+cd ../../../dist/"$1"/mac
+./plugin_base.ref_gen firefly_synth_1.vst3/Contents/MacOS/firefly_synth_1.so ../../../param_reference.html
+cd ../../../scripts
