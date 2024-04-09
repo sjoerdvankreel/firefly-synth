a156a0e888a0e9a09c84ee9a533b3e45fba5129b
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-04-09 19:20:13 +0200

    take 108

--- src/firefly_synth/firefly_synth/synth.cpp
+++ src/firefly_synth/firefly_synth/synth.cpp
@@ -205,7 +205,7 @@ make_title_text_section(plugin_gui* gui, lnf* lnf, component_store store, bool i
   auto& grid = store_component<grid_component>(store, gui_dimension({ { 2, 1 }, { 1 } }), 2, 2, 0, 1);
   auto& title_label = store_component<autofit_label>(store, lnf, name, true, 15);
   title_label.setColour(Label::ColourIds::textColourId, colors.control_text);
-  title_label.setJustificationType(Justification::centredRight);
+  title_label.setJustificationType(Justification::centred);
   grid.add(title_label, { 0, 0, 1, 1 });
   std::string version_text = std::string(FF_SYNTH_VERSION_TEXT) + " " + gui->gui_state()->desc().config->format_name() + " ";
 #ifdef __aarch64__
