e92e4564f492a06fb705b2fe685b790e0a0138cb
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-04-13 22:21:25 +0200

    take 44

--- plugin_base/src/plugin_base/plugin_base/gui/lnf.cpp
+++ plugin_base/src/plugin_base/plugin_base/gui/lnf.cpp
@@ -513,7 +513,7 @@ void
 lnf::drawButtonBackground(
   Graphics& g, Button& button, Colour const& backgroundColour, 
   bool shouldDrawButtonAsHighlighted, bool shouldDrawButtonAsDown)
-{                   
+{                    
   // this is a 1:1 copy of LookAndFeel_V4::drawButtonBackground with 2 modifications: 
   // 1: we pick the corner size from theme_settings, whereas base class hardcodes it as 6
   // 2: reduce local bounds by more than 0.5
