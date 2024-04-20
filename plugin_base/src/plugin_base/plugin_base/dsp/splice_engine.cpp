0f8322b3b7e1e4a36afad605230ac8a4468f8163
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-04-20 23:06:05 +0200

    take 24

--- plugin_base/src/plugin_base/plugin_base/dsp/splice_engine.cpp
+++ plugin_base/src/plugin_base/plugin_base/dsp/splice_engine.cpp
@@ -1,90 +1,6 @@
 #include <plugin_base/dsp/splice_engine.hpp>
 
 namespace plugin_base {
-
-// baseline = ~250mb, with each 64 samples is ~100mb of extra data
-// should investigate
-
-// with all route count set to 4: baseline = 220
-// each extra 64 samples = 100
-
-// with polyphony set to 4: baseline = 80
-// each extra 64 samples = 4
-
-// ok so its not the automation buffers since those are not per-voice
-// maybe the oversamplers ?
-
-// with no changes but oversampling: 
-// baseline = 220
-// each extra 64 samples = 100
-
-// don't activate() and dont activate_modules()
-// baseline = 50 
-// extra 64 = 0
-
-// activate() but dont activate_modules()
-// baseline = 60
-// extra 64 = 20
-
-// ok so it's one of the per-voice modules
-// investigate more
-
-// 2 osc and 2 vfx
-// baseline = 150
-// extra 64  = 70
-
-// no changes but max_osc_unison_voices = 2
-// baseline = 180
-// extra 64 = 60
-
-// no changes but env = 2
-// base = 220
-// extra 64  = 100
-
-// just dont activate per-voice modules
-// base = 100
-// extra 64 = 18 
-
-// ----
-// ----
-
-// baseline = ~250mb, with each 64 samples is ~100mb of extra data
-// should investigate
-
-// set osc count to 2
-// base = 190
-// extra 64 = 70 
-// osc = 20 + 10
-
-// and set vfx count to 2
-// base = 160
-// extra 64 = 60
-// vfx = 4 + 1
-
-// and set vlfo count to 2
-// base = 150
-// extra 64 = 60
-// vlfo = 1 + 0
-
-// and set venv count to 2
-// base = 150
-// extra 64 = 60
-// venv = 0 + 0
-
-// total voice size on 64 samples = 
-// 5 * osc = 150
-// 10 * fx = 50
-// = 200
-
-// without oversampling
-// block 4 = 200 
-// block 64 = 230
-// 30 mb block size = good!
-
-// with max oversampling = 4x
-// block 4 = 210 
-// block 64 = 270
-// 60 mb block size is mweh
   
 plugin_splice_engine::
 plugin_splice_engine(
@@ -92,7 +8,8 @@ plugin_splice_engine(
   thread_pool_voice_processor voice_processor,
   void* voice_processor_context):
 _engine(desc, graph, voice_processor, voice_processor_context),
-_splice_block_size(desc->plugin->splice_block_size) {}
+_splice_block_size(desc->plugin->splice_block_size)
+{ assert(_splice_block_size >= 64); }
 
 host_block&
 plugin_splice_engine::prepare_block()
@@ -118,7 +35,73 @@ plugin_splice_engine::activate(int max_frame_count)
 void
 plugin_splice_engine::process()
 {
+  // TODO breakpoint this
+  // process in blocks of splice_size/2, with any remaining allocated to the last block
+  int min_block_frames = _splice_block_size / 2;
+  int min_block_count = _host_block.frame_count / min_block_frames;
+  int rest_block_frames = _host_block.frame_count - min_block_count * min_block_frames;
+  assert(min_block_count * min_block_frames + rest_block_frames == _host_block.frame_count);
+  int total_block_count = min_block_count + (rest_block_frames == 0? 0: 1);
 
+  _host_block.events.out.clear();
+  for (int i = 0; i < total_block_count; i++)
+  {
+    int this_block_start = i * min_block_frames;
+    int this_block_frames = i < min_block_count? min_block_frames: rest_block_frames;
+
+    auto& inner_block = _engine.prepare_block();
+    inner_block.frame_count = _host_block.frame_count;
+    inner_block.shared.bpm = _host_block.shared.bpm;
+
+    float* this_audio_out[2];
+    this_audio_out[0] = _host_block.audio_out[0] + this_block_start;
+    this_audio_out[1] = _host_block.audio_out[1] + this_block_start;
+    inner_block.audio_out = this_audio_out;
+
+    float const* this_audio_in[2];
+    this_audio_in[0] = _host_block.shared.audio_in[0] + this_block_start;
+    this_audio_in[1] = _host_block.shared.audio_in[1] + this_block_start;
+    inner_block.shared.audio_in = this_audio_in;
+
+    inner_block.events.midi.clear();
+    inner_block.events.block.clear();
+    inner_block.events.notes.clear();
+    inner_block.events.accurate.clear();
+
+    // this could be made more efficient but i doubt if it would matter
+
+    for (int b = 0; b < _host_block.events.block.size(); b++)
+      inner_block.events.block.push_back(_host_block.events.block[b]);
+    for(int m = 0; m < _host_block.events.midi.size(); m++)
+      if(_host_block.events.midi[m].frame >= this_block_start && _host_block.events.midi[m].frame < this_block_start + this_block_frames)
+      {
+        midi_event e = _host_block.events.midi[m];
+        e.frame -= this_block_start;
+        inner_block.events.midi.push_back(e);
+      }
+    for (int a = 0; a < _host_block.events.accurate.size(); a++)
+      if (_host_block.events.accurate[a].frame >= this_block_start && _host_block.events.accurate[a].frame < this_block_start + this_block_frames)
+      {
+        accurate_event e = _host_block.events.accurate[a];
+        e.frame -= this_block_start;
+        inner_block.events.accurate.push_back(e);
+      }
+    for (int n = 0; n < _host_block.events.notes.size(); n++)
+      if (_host_block.events.notes[n].frame >= this_block_start && _host_block.events.notes[n].frame < this_block_start + this_block_frames)
+      {
+        note_event e = _host_block.events.notes[n];
+        e.frame -= this_block_start;
+        inner_block.events.notes.push_back(e);
+      }
+
+    _engine.process();
+
+    // copy last out block events
+    if(i == total_block_count - 1)
+      std::copy(inner_block.events.out.begin(), inner_block.events.out.end(), _host_block.events.out.begin());
+
+    _engine.release_block();
+  }
 }
 
 }
