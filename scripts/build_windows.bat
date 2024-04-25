f42f58e20935e5fd8b7a6daf57153236fec10ec5
ofsjoerdk <Sjoerd.vanKreel@ortec-finance.com>
2024-04-25 21:18:11 +0200

    take 1

--- scripts/build_windows.bat
+++ scripts/build_windows.bat
@@ -14,10 +14,10 @@ cd ..\..\dist\"%1"\win
 plugin_base.ref_gen.exe firefly_synth_1.vst3\Contents\x86_64-win\firefly_synth_1.vst3 ..\..\..\param_reference.html
 if %errorlevel% neq 0 exit /b !errorlevel!
 
-7z a -tzip firefly_synth_1.7.2_windows_vst3_fx.zip firefly_synth_fx_1.vst3\*
-7z a -tzip firefly_synth_1.7.2_windows_vst3_instrument.zip firefly_synth_1.vst3\*
-7z a -tzip firefly_synth_1.7.2_windows_clap_fx.zip firefly_synth_fx_1.clap\*
-7z a -tzip firefly_synth_1.7.2_windows_clap_instrument.zip firefly_synth_1.clap\*
+7z a -tzip firefly_synth_1.7.3_windows_vst3_fx.zip firefly_synth_fx_1.vst3\*
+7z a -tzip firefly_synth_1.7.3_windows_vst3_instrument.zip firefly_synth_1.vst3\*
+7z a -tzip firefly_synth_1.7.3_windows_clap_fx.zip firefly_synth_fx_1.clap\*
+7z a -tzip firefly_synth_1.7.3_windows_clap_instrument.zip firefly_synth_1.clap\*
 if %errorlevel% neq 0 exit /b !errorlevel!
 
 cd ..\..\..\..\scripts
