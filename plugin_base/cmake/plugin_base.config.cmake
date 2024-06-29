fae68cf03d0943b46bb971e2000c587c9f8bdbe4
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-06-29 20:01:16 +0200

    msvcrt static take 1

--- plugin_base/cmake/plugin_base.config.cmake
+++ plugin_base/cmake/plugin_base.config.cmake
@@ -1,7 +1,7 @@
 cmake_policy(SET CMP0091 NEW)
 
 if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-add_compile_options(/W4;/WX;/external:W0;/wd4245;/wd4267;/wd4244;/wd4100;/wd26495;/wd4389;/fp:fast;/arch:AVX)
+add_compile_options(/W4;/WX;/external:W0;/wd4245;/wd4267;/wd4244;/wd4100;/wd26495;/wd4389;/fp:fast;/arch:AVX;$<$<CONFIG:Debug>:/MTd>$<$<CONFIG:Release>:/MT>$<$<CONFIG:RelWithDebInfo>:/MT>)
 elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU") 
 add_compile_options(-Wl,--no-undefined -fvisibility=hidden -fvisibility-inlines-hidden)
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wl,--no-undefined -fvisibility=hidden -fvisibility-inlines-hidden")
