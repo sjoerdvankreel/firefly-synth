118ec68e99624f1afbf4afb22d04596bf581e682
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-07-17 12:26:02 +0200

    take 54

--- plugin_base/src/plugin_base/plugin_base/gui/utility.cpp
+++ plugin_base/src/plugin_base/plugin_base/gui/utility.cpp
@@ -30,20 +30,25 @@ color_to_grayscale(juce::Colour const& c)
 juce::ScaledImage
 make_drag_source_image(Font const& font, std::string const& text, Colour border_color)
 {
+  // DragAndDropContainer::startDragging, imageOffsetFromMouse parameter seems not working
+  // so fake it here
+  float extra_left = 10;
+  float extra_top = 10;
+
   float margin_w = 8.0f;
   float margin_h = 4.0f;
   float text_h = font.getHeight();
   float text_w = font.getStringWidthFloat(String(text));
 
-  Image image(Image::PixelFormat::ARGB, text_w + margin_w, text_h + margin_h, true);
+  Image image(Image::PixelFormat::ARGB, text_w + margin_w + extra_left, text_h + margin_h + extra_top, true);
   Graphics g(image);
   g.setFont(font);
   g.setColour(Colours::black);
-  g.fillRect(0.0f, 0.0f, text_w + margin_w - 1, text_h + margin_h);
+  g.fillRect(extra_left, extra_top, text_w + margin_w - 1, text_h + margin_h);
   g.setColour(border_color);
-  g.drawRect(Rectangle<float>(0.0f, 0.0f, text_w + margin_w - 1, text_h + margin_h).reduced(0.5f), 1.0f);
+  g.drawRect(Rectangle<float>(extra_left, extra_top, text_w + margin_w - 1, text_h + margin_h).reduced(0.5f), 1.0f);
   g.setColour(Colours::white);
-  g.drawText(text, margin_w / 2, margin_h / 2, text_w, text_h, Justification::centredBottom, false);
+  g.drawText(text, extra_left + margin_w / 2, extra_top + margin_h / 2, text_w, text_h, Justification::centredBottom, false);
   return ScaledImage(image);
 }
 
