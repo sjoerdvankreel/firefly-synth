16dfba3f8f79e189e2ede5cb5a93b09a5a6f13e3
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-08-20 22:51:11 +0200

    take 12

--- plugin_base/src/plugin_base/plugin_base/gui/lnf.cpp
+++ plugin_base/src/plugin_base/plugin_base/gui/lnf.cpp
@@ -827,12 +827,22 @@ lnf::drawRotarySlider(Graphics& g, int, int, int, int, float pos, float, float, 
       Path path;
       g.setColour(colors().slider_mod_indicator);
 
-      if(!bipolar)
-        path.addArc(left - 3, top - 3, size + 6, size + 6, start_angle, start_angle + ps->max_mod_indicator() * angle_range, true);
+      float half_mod_angle = start_angle + 0.5f * angle_range;
+      float min_mod_angle = start_angle + ps->min_mod_indicator() * angle_range;
+      float max_mod_angle = start_angle + ps->max_mod_indicator() * angle_range;
+      if (!bipolar)
+      {
+        if(ps->max_mod_indicator() == ps->min_mod_indicator())
+          path.addArc(left - 3, top - 3, size + 6, size + 6, start_angle, max_mod_angle, true);
+        else
+          path.addArc(left - 3, top - 3, size + 6, size + 6, min_mod_angle, max_mod_angle, true);
+      }
+      else if(ps->max_mod_indicator() != ps->min_mod_indicator())
+        path.addArc(left - 3, top - 3, size + 6, size + 6, min_mod_angle, max_mod_angle, true);
       else if(ps->max_mod_indicator() >= 0.5f)
-        path.addArc(left - 3, top - 3, size + 6, size + 6, start_angle + 0.5f * angle_range, start_angle + ps->max_mod_indicator() * angle_range, true);
+        path.addArc(left - 3, top - 3, size + 6, size + 6, half_mod_angle, max_mod_angle, true);
       else
-        path.addArc(left - 3, top - 3, size + 6, size + 6, start_angle + ps->max_mod_indicator() * angle_range, start_angle + 0.5f * angle_range, true);
+        path.addArc(left - 3, top - 3, size + 6, size + 6, max_mod_angle, half_mod_angle, true);
 
       g.strokePath(path, PathStrokeType(2));
     }
