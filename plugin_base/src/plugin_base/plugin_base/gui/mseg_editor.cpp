f909276275356db7c731b2db9c56d40d52f990fc
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-10-28 19:49:51 +0100

    take 74

--- plugin_base/src/plugin_base/plugin_base/gui/mseg_editor.cpp
+++ plugin_base/src/plugin_base/plugin_base/gui/mseg_editor.cpp
@@ -227,27 +227,27 @@ mseg_editor::itemDragMove(juce::DragAndDropTarget::SourceDetails const& details)
   float const y = padding;
   //float const w = getLocalBounds().getWidth() - padding * 2.0f;
   float const h = getLocalBounds().getHeight() - padding * 2.0f;
-  auto const state = _gui->automation_state();
 
   float drag_y_amt = 1.0f - std::clamp((details.localPosition.y - y) / h, 0.0f, 1.0f);;
 
   if (_dragging_start_y)
   {
-    state->set_raw_at(_module_index, _module_slot, _start_y_param, 0, drag_y_amt);
+    // todo start/end
+    _gui->param_changed(_module_index, _module_slot, _start_y_param, 0, drag_y_amt);
     repaint();
     return;
   }
 
   if (_dragging_end_y)
   {
-    state->set_raw_at(_module_index, _module_slot, _end_y_param, 0, drag_y_amt);
+    _gui->param_changed(_module_index, _module_slot, _end_y_param, 0, drag_y_amt);
     repaint();
     return;
   }
 
   if (_dragging_point != -1)
   {
-    state->set_raw_at(_module_index, _module_slot, _y_param, _dragging_point, drag_y_amt);
+    _gui->param_changed(_module_index, _module_slot, _y_param, _dragging_point, drag_y_amt);
     repaint();
     return;
   }
