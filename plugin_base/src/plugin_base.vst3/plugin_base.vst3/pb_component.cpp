3ba8159c7f868fa1ae8b2d01a70b985a0ba39a69
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-11-10 20:04:12 +0100

    stuff

--- plugin_base/src/plugin_base.vst3/plugin_base.vst3/pb_component.cpp
+++ plugin_base/src/plugin_base.vst3/plugin_base.vst3/pb_component.cpp
@@ -240,7 +240,12 @@ pb_component::process(ProcessData& data)
     auto num_modulation_outputs = block.events.modulation_outputs.size();
     num_modulation_outputs = std::min((int)num_modulation_outputs, modulation_output_param_count);
     queue = data.outputParameterChanges->addParameterData(_mod_output_count_param_tag, unused_index);
-    queue->addPoint(0, *reinterpret_cast<double*>(&num_modulation_outputs), unused_index);
+    
+    // make it debuggable so we can see what comes out on the other side, updated reaper is not digging the type punning
+    // oh god its denormal -- are we FTZ/DAZ now?
+    auto num_mod_outputs_as_double = *reinterpret_cast<double*>(&num_modulation_outputs);
+
+    queue->addPoint(0, num_mod_outputs_as_double, unused_index);
     for (int e = 0; e < block.events.modulation_outputs.size(); e++)
     {
       auto const& this_modulation_outputs = block.events.modulation_outputs[e];
